7ab5995663c8830f863bce3e855c230787dcd274
ha6-6ru <20640973+ha6-6ru@users.noreply.github.com>
2022-02-14 13:31:52 +0900

    add store template

---
+++ samples/Dressca/dressca-frontend/src/stores/authentication/authentication.ts
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia';
+
+export const useAuthenticationStore = defineStore({
+  id: 'authentication',
+  state: () => ({}),
+  actions: {},
+  getters: {},
+});
---
+++ samples/Dressca/dressca-frontend/src/stores/catalog/catalog.ts
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia';
+
+export const useCatalogStore = defineStore({
+  id: 'catalog',
+  state: () => ({}),
+  actions: {},
+  getters: {},
+});
