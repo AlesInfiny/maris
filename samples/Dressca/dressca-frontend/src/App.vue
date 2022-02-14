62e49e50959e2bb70902753992238d2f5fb6710b
ha6-6ru <20640973+ha6-6ru@users.noreply.github.com>
2022-02-14 21:48:42 +0900

    add frame, basket

--- samples/Dressca/dressca-frontend/src/App.vue
+++ samples/Dressca/dressca-frontend/src/App.vue
@@ -1,121 +1,36 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { ShoppingCartIcon } from '@heroicons/vue/solid';
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <div class="flex flex-col h-screen justify-between">
+    <header>
+      <nav
+        aria-label="Jump links"
+        class="sticky top-0 z-40 bg-white text-lg font-medium text-gray-900 py-5 ring-1 ring-gray-900 ring-opacity-5 shadow-sm"
+      >
+        <div class="max-w-container mx-auto flex justify-between px-24">
+          <div>
+            <router-link class="text-2xl" to="/">Dressca</router-link>
+          </div>
+          <div class="flex space-x-5 sm:space-x-8 lg:space-x-12">
+            <router-link to="/basket">
+              <ShoppingCartIcon class="h-8 w-8 text-amber-600" />
+            </router-link>
+            <router-link to="/account/login">ログイン</router-link>
+          </div>
+        </div>
       </nav>
-    </div>
-  </header>
+    </header>
 
-  <RouterView />
+    <main class="mb-auto">
+      <router-view />
+    </main>
+
+    <footer
+      class="w-full max-w-container mx-auto border-t py-4 px-24 text-base bg-black text-gray-500"
+    >
+      <p>&copy; 2020 - Dressca - Privacy</p>
+    </footer>
+  </div>
 </template>
-
-<style lang="postcss">
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsl(160deg 100% 37% / 100%);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsl(160deg 100% 37% / 20%);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
