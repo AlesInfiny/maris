bb22a37a88ff5145761e6feadfed02ab61b94fff
ha6-6ru <20640973+ha6-6ru@users.noreply.github.com>
2022-02-15 16:43:40 +0900

    remove miragejs, impl web api mock

---
+++ samples/Dressca/dressca-frontend/src/stores/catalog/catalog.model.ts
@@ -0,0 +1,18 @@
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Brand {
+  id: number;
+  name: string;
+}
+
+export interface Item {
+  catalogCategoryId: number;
+  catalogBrandId: number;
+  description: string;
+  name: string;
+  price: number;
+  productCode: string;
+}
--- samples/Dressca/dressca-frontend/vite.config.ts
+++ samples/Dressca/dressca-frontend/vite.config.ts
@@ -3,13 +3,18 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { setupMockPlugin } from './vite-plugins/setup-mock';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const plugins = [vue(), vueJsx()];
+
+  return {
+    plugins: mode === 'mock' ? [...plugins, setupMockPlugin()] : plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  };
 });
