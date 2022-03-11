242f901097ea179c82443187bd98ce4384b872bb
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-12 00:37:26 +0900

    カタログリポジトリのEFCore実装を追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.EfInfrastructure/EfCatalogCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dressca.ApplicationCore.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dressca.EfInfrastructure;
+
+/// <summary>
+///  Entity Framework Core を利用したカタログカテゴリリポジトリの実装です。
+/// </summary>
+internal class EfCatalogCategoryRepository : ICatalogCategoryRepository
+{
+    private readonly DresscaDbContext dbContext;
+
+    /// <summary>
+    ///  データアクセスに使用する <see cref="DresscaDbContext"/> を指定して
+    ///  <see cref="EfCatalogCategoryRepository"/> クラスの新しいインスタンスを初期化します。
+    /// </summary>
+    /// <param name="dbContext">データアクセスに使用する <see cref="DresscaDbContext"/> オブジェクト。</param>
+    /// <exception cref="ArgumentNullException">
+    ///  <paramref name="dbContext"/> が <see langword="null"/> です。
+    /// </exception>
+    public EfCatalogCategoryRepository(DresscaDbContext dbContext)
+        => this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    /// <inheritdoc/>
+    public async Task<IReadOnlyList<CatalogCategory>> GetAllAsync(CancellationToken cancellationToken = default)
+        => await this.dbContext.CatalogCategories.ToListAsync(cancellationToken);
+}
