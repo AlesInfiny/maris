6c5c644b2b9041da300bca322c30842dbbca1054
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-12 00:20:09 +0900

    買い物かご機能のエンティティを追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Baskets/Basket.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Dressca.ApplicationCore.Baskets;
+
+/// <summary>
+///  買い物かごエンティティ。
+/// </summary>
+public class Basket
+{
+    private readonly List<BasketItem> items = new();
+    private string buyerId;
+
+    /// <summary>
+    ///  <see cref="Basket"/> クラスの新しいインスタンスを初期化します。
+    /// </summary>
+    /// <param name="buyerId">購入者 Id 。</param>
+    /// <exception cref="ArgumentNullException">
+    ///  <paramref name="buyerId"/> が <see langword="null"/> です.
+    /// </exception>
+    public Basket(string buyerId) => this.BuyerId = buyerId;
+
+    /// <summary>
+    ///  買い物かご Id を取得します。
+    /// </summary>
+    public long Id { get; init; }
+
+    /// <summary>
+    ///  購入者 Id を取得します。
+    /// </summary>
+    public string BuyerId
+    {
+        get => this.buyerId;
+
+        [MemberNotNull(nameof(buyerId))]
+        private set
+        {
+            this.buyerId = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
+
+    /// <summary>
+    ///  買い物かごアイテムを取得します。
+    /// </summary>
+    public IReadOnlyCollection<BasketItem> Items => this.items.AsReadOnly();
+
+    /// <summary>
+    ///  買い物かごにアイテムを追加します。
+    ///  同一アイテムが既に買い物かご内に存在する場合は、<paramref name="quantity"/> 分だけ数量が加算されます。
+    ///  またこのとき、<paramref name="quantity"/> が負の値の場合は、数量を減算します。
+    /// </summary>
+    /// <param name="catalogItemId">カタログアイテム Id 。</param>
+    /// <param name="unitPrice">単価。</param>
+    /// <param name="quantity">数量。</param>
+    /// <exception cref="InvalidOperationException">アイテムの数量が負値になる場合。</exception>
+    public void AddItem(long catalogItemId, decimal unitPrice, int quantity = 1)
+    {
+        if (!this.items.Any(i => i.CatalogItemId == catalogItemId))
+        {
+            this.items.Add(new BasketItem(catalogItemId, unitPrice, quantity));
+            return;
+        }
+
+        var existingItem = this.items.First(i => i.CatalogItemId == catalogItemId);
+        existingItem.AddQuantity(quantity);
+    }
+
+    /// <summary>
+    ///  数量が 0 のアイテムを買い物かごから除去します。
+    /// </summary>
+    public void RemoveEmptyItems() => _ = this.items.RemoveAll(i => i.Quantity == 0);
+
+    /// <summary>
+    ///  この買い物かごの購入者 Id を変更します。
+    /// </summary>
+    /// <param name="buyerId">購入者 Id 。</param>
+    /// <exception cref="ArgumentNullException">
+    ///  <paramref name="buyerId"/> が <see langword="null"/> です.
+    /// </exception>
+    public void SetNewBuyerId(string buyerId) => this.BuyerId = buyerId;
+}
---
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Baskets/BasketItem.cs
@@ -0,0 +1,88 @@
+﻿using Dressca.ApplicationCore.Resources;
+
+namespace Dressca.ApplicationCore.Baskets;
+
+/// <summary>
+///  買い物かごアイテムです。
+/// </summary>
+public class BasketItem
+{
+    private Basket? basket;
+    private int quantity;
+
+    /// <summary>
+    ///  <see cref="BasketItem"/> クラスの新しいインスタンスを初期化します。
+    /// </summary>
+    /// <param name="catalogItemId">カタログアイテム Id 。</param>
+    /// <param name="unitPrice">単価。</param>
+    /// <param name="quantity">数量。</param>
+    /// <exception cref="ArgumentException"><paramref name="quantity"/> が 0 未満の場合。</exception>
+    public BasketItem(long catalogItemId, decimal unitPrice, int quantity)
+    {
+        this.CatalogItemId = catalogItemId;
+        this.UnitPrice = unitPrice;
+        this.Quantity = quantity;
+    }
+
+    /// <summary>
+    ///  買い物かごアイテム Id を取得します。
+    /// </summary>
+    public long Id { get; init; }
+
+    /// <summary>
+    ///  買い物かご（ナビゲーションプロパティ）を取得します。
+    /// </summary>
+    /// <exception cref="InvalidOperationException"><see cref="Basket"/> が設定されていません。</exception>
+    public Basket Basket
+    {
+        get => this.basket ?? throw new InvalidOperationException(string.Format(ApplicationCoreMessages.PropertyNotInitialized, nameof(this.Basket)));
+        private set => this.basket = value;
+    }
+
+    /// <summary>
+    ///  買い物かご Id を取得します。
+    /// </summary>
+    public long BasketId { get; private set; }
+
+    /// <summary>
+    ///  カタログアイテム Id を取得します。
+    /// </summary>
+    public long CatalogItemId { get; private set; }
+
+    /// <summary>
+    ///  単価を取得します。
+    /// </summary>
+    public decimal UnitPrice { get; private set; }
+
+    /// <summary>
+    ///  数量を取得します。
+    /// </summary>
+    /// <exception cref="ArgumentException">0 未満を設定した場合。</exception>
+    public int Quantity
+    {
+        get => this.quantity;
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(ApplicationCoreMessages.BasketItemQuantityMustBePositive, nameof(value));
+            }
+
+            this.quantity = value;
+        }
+    }
+
+    /// <summary>
+    ///  数量を <paramref name="quantity"/> 分加算します。
+    /// </summary>
+    /// <param name="quantity">加算する数量。</param>
+    /// <exception cref="ArgumentException">加算後の数量が負になる場合。オーバーフローする場合も同様。</exception>
+    public void AddQuantity(int quantity) => this.SetQuantity(this.Quantity + quantity);
+
+    /// <summary>
+    ///  数量を設定します。
+    /// </summary>
+    /// <param name="quantity">数量。</param>
+    /// <exception cref="ArgumentException"><paramref name="quantity"/> が 0 未満の場合。</exception>
+    public void SetQuantity(int quantity) => this.Quantity = quantity;
+}
