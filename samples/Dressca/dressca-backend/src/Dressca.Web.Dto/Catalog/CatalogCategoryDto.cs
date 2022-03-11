ebe30cb71bc55950296c3101ac5ec3facc5d02ca
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-12 00:42:23 +0900

    カタログカテゴリのマッパーとDTOを追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.Web.Dto/Catalog/CatalogCategoryDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dressca.Web.Dto.Catalog;
+
+/// <summary>
+///  カタログカテゴリの DTO です。
+/// </summary>
+public class CatalogCategoryDto
+{
+    /// <summary>
+    ///  カタログカテゴリ Id を取得または設定します。
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    ///  カテゴリ名を取得または設定します。
+    /// </summary>
+    [Required]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ samples/Dressca/dressca-backend/src/Dressca.Web/Mapper/CatalogCategoryMapper.cs
@@ -0,0 +1,33 @@
+﻿using Dressca.ApplicationCore.Catalog;
+using Dressca.SystemCommon.Mapper;
+using Dressca.Web.Dto.Catalog;
+
+namespace Dressca.Web.Mapper;
+
+/// <summary>
+///  <see cref="CatalogCategory"/> と <see cref="CatalogCategoryDto"/> のマッパーです。
+/// </summary>
+public class CatalogCategoryMapper : IObjectMapper<CatalogCategory, CatalogCategoryDto>
+{
+    /// <inheritdoc/>
+    public CatalogCategory? Convert(CatalogCategoryDto? value)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        return new(value.Name) { Id = value.Id };
+    }
+
+    /// <inheritdoc/>
+    public CatalogCategoryDto? Convert(CatalogCategory? value)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        return new() { Id = value.Id, Name = value.Name };
+    }
+}
