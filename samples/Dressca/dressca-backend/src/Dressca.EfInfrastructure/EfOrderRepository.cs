7fabfcee11dd74fff174ea6751fff950a24d8fcd
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-12 00:37:37 +0900

    注文リポジトリのEFCore実装を追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.EfInfrastructure/EfOrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dressca.ApplicationCore.Ordering;
+
+namespace Dressca.EfInfrastructure;
+
+/// <summary>
+///  Entity Framework Core を利用した注文リポジトリの実装です。
+/// </summary>
+internal class EfOrderRepository : IOrderRepository
+{
+    private readonly DresscaDbContext dbContext;
+
+    /// <summary>
+    ///  データアクセスに使用する <see cref="DresscaDbContext"/> を指定して
+    ///  <see cref="EfOrderRepository"/> クラスの新しいインスタンスを初期化します。
+    /// </summary>
+    /// <param name="dbContext">データアクセスに使用する <see cref="DresscaDbContext"/> オブジェクト。</param>
+    /// <exception cref="ArgumentNullException">
+    ///  <paramref name="dbContext"/> が <see langword="null"/> です。
+    /// </exception>
+    public EfOrderRepository(DresscaDbContext dbContext)
+        => this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    /// <inheritdoc/>
+    public async Task<Order> AddAsync(Order entity, CancellationToken cancellationToken = default)
+    {
+        this.dbContext.Orders.Add(entity);
+        _ = await this.dbContext.SaveChangesAsync(cancellationToken);
+        return entity;
+    }
+}
