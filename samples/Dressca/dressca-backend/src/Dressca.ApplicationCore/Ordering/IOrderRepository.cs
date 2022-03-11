7d116b641a4f1339a8f696938d56391b29ae8ba9
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-12 00:30:01 +0900

    注文リポジトリのインターフェースを追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Ordering/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Dressca.ApplicationCore.Ordering;
+
+/// <summary>
+///  注文リポジトリ。
+/// </summary>
+public interface IOrderRepository
+{
+    /// <summary>
+    ///  エンティティを追加します。
+    /// </summary>
+    /// <param name="entity">エンティティ。</param>
+    /// <param name="cancellationToken">キャンセルトークン。</param>
+    /// <returns>追加された注文情報を返す非同期処理を表すタスク。</returns>
+    Task<Order> AddAsync(Order entity, CancellationToken cancellationToken = default);
+}
