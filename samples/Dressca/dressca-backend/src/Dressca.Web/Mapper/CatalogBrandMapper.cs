cc7f982bcb0d2a70d7791bfdc9956fd622495328
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-12 00:42:03 +0900

    カタログブランドのマッパーとDTOを追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.Web/Mapper/CatalogBrandMapper.cs
@@ -0,0 +1,33 @@
+﻿using Dressca.ApplicationCore.Catalog;
+using Dressca.SystemCommon.Mapper;
+using Dressca.Web.Dto.Catalog;
+
+namespace Dressca.Web.Mapper;
+
+/// <summary>
+///  <see cref="CatalogBrand"/> と <see cref="CatalogBrandDto"/> のマッパーです。
+/// </summary>
+public class CatalogBrandMapper : IObjectMapper<CatalogBrand, CatalogBrandDto>
+{
+    /// <inheritdoc/>
+    public CatalogBrand? Convert(CatalogBrandDto? value)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        return new(value.Name) { Id = value.Id };
+    }
+
+    /// <inheritdoc/>
+    public CatalogBrandDto? Convert(CatalogBrand? value)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        return new() { Id = value.Id, Name = value.Name };
+    }
+}
