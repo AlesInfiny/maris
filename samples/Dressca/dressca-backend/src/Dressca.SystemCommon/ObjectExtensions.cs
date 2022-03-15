180abb2eac06fbadba64f904f315c7760c9dcc3c
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-15 13:52:30 +0900

    オブジェクトがnullか確認するシステム共通メソッドを追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.SystemCommon/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Dressca.SystemCommon;
+
+/// <summary>
+///  <see cref="object"/> クラスの拡張メソッドを提供します。
+/// </summary>
+public static class ObjectExtensions
+{
+    /// <summary>
+    ///  指定した値が <see langword="null"/> であるか検証し、
+    ///  <see langword="null"/> である場合は例外をスローします。
+    /// </summary>
+    /// <param name="obj"><see langword="null"/> か検査するオブジェクト。</param>
+    /// <exception cref="ArgumentNullException">
+    ///  <paramref name="obj"/> が <see langword="null"/> です。
+    /// </exception>
+    public static void ThrowIfNull([NotNull] this object? obj)
+    {
+        if (obj is null)
+        {
+            throw new ArgumentNullException(nameof(obj));
+        }
+    }
+}
