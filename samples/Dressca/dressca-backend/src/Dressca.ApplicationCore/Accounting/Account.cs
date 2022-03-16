959e77d4dc061917d490b56f73b18f50df7001f0
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-16 15:54:48 +0900

    会計処理を行う値オブジェクトを追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Accounting/Account.cs
@@ -0,0 +1,58 @@
+﻿namespace Dressca.ApplicationCore.Accounting;
+
+/// <summary>
+///  会計情報を表現する値オブジェクトです。
+/// </summary>
+public record Account
+{
+    private readonly IEnumerable<AccountItem> accountItems;
+
+    /// <summary>
+    ///  <see cref="Account"/> クラスの新しいインスタンスを初期化します。
+    /// </summary>
+    /// <param name="accountItems">会計アイテムのリスト。</param>
+    /// <exception cref="ArgumentNullException">
+    ///  <paramref name="accountItems"/> が <see langword="null"/> です。
+    /// </exception>
+    public Account(IEnumerable<AccountItem> accountItems)
+        => this.accountItems = accountItems ?? throw new ArgumentNullException(nameof(accountItems));
+
+    /// <summary>
+    ///  消費税率を取得します。
+    ///  簡単化のため、本サンプルでは 10% 固定です。
+    /// </summary>
+    public static decimal ConsumptionTaxRate => 0.1m;
+
+    /// <summary>
+    ///  会計アイテムの合計金額を取得します。
+    ///  この計算結果には、消費税や送料は含まれません。
+    /// </summary>
+    /// <returns>会計アイテムの合計金額。</returns>
+    public decimal GetItemsTotalPrice()
+        => this.accountItems.Sum(item => item.Quantity * item.UnitPrice);
+
+    /// <summary>
+    ///  税抜きの送料を取得します。
+    ///  送料は会計アイテムの合計金額が 5,000 円以上で無料になります。
+    ///  それ以外の場合 500 円です。
+    /// </summary>
+    /// <returns>送料。</returns>
+    public decimal GetDeliveryCharge()
+        => this.GetItemsTotalPrice() >= 5000m ? 0m : 500m;
+
+    /// <summary>
+    ///  消費税額の合計を取得します。
+    ///  会計アイテムの合計金額に送料を加えた額に、消費税率を乗じて計算します。
+    ///  0 円未満の端数は切り捨てます。
+    /// </summary>
+    /// <returns>消費税額。</returns>
+    public decimal GetConsumptionTax()
+        => Math.Truncate((this.GetItemsTotalPrice() + this.GetDeliveryCharge()) * ConsumptionTaxRate);
+
+    /// <summary>
+    ///  税込みの合計金額を取得します。
+    /// </summary>
+    /// <returns>合計金額。</returns>
+    public decimal GetTotalPrice()
+        => this.GetItemsTotalPrice() + this.GetDeliveryCharge() + this.GetConsumptionTax();
+}
---
+++ samples/Dressca/dressca-backend/tests/Dressca.UnitTests/ApplicationCore/Accounting/AccountTest.cs
@@ -0,0 +1,121 @@
+﻿using Dressca.ApplicationCore.Accounting;
+
+namespace Dressca.UnitTests.ApplicationCore.Accounting;
+
+public class AccountTest
+{
+    [Fact]
+    public void 会計アイテムがないとき会計アイムの合計金額は0円()
+    {
+        // Arrange
+        var account = new Account(Array.Empty<AccountItem>());
+
+        // Act
+        var itemsTotal = account.GetItemsTotalPrice();
+
+        // Assert
+        Assert.Equal(0m, itemsTotal);
+    }
+
+    [Fact]
+    public void 会計アイテムが1件あるとき会計アイムの合計金額は送料と消費税をのぞく合計金額になる()
+    {
+        // Arrange
+        var accountItems = new AccountItem[] { new(5, 300m) };
+        var account = new Account(accountItems);
+
+        // Act
+        var itemsTotal = account.GetItemsTotalPrice();
+
+        // Assert
+        Assert.Equal(1500m, itemsTotal);
+    }
+
+    [Fact]
+    public void 会計アイテムが2件あるとき会計アイムの合計金額は送料と消費税をのぞく合計金額になる()
+    {
+        // Arrange
+        var accountItems = new AccountItem[]
+        {
+            new(5, 300m),
+            new(2, 400m),
+        };
+        var account = new Account(accountItems);
+
+        // Act
+        var itemsTotal = account.GetItemsTotalPrice();
+
+        // Assert
+        Assert.Equal(2300m, itemsTotal);
+    }
+
+    [Fact]
+    public void 会計アイムの合計金額が5000円未満の場合は送料が500円になる()
+    {
+        // Arrange
+        var accountItems = new AccountItem[] { new(1, 4999m) };
+        var account = new Account(accountItems);
+
+        // Act
+        var deliveryCharge = account.GetDeliveryCharge();
+
+        // Assert
+        Assert.Equal(500m, deliveryCharge);
+    }
+
+    [Fact]
+    public void 会計アイムの合計金額が5000円以上の場合は送料が0円になる()
+    {
+        // Arrange
+        var accountItems = new AccountItem[] { new(1, 5000m) };
+        var account = new Account(accountItems);
+
+        // Act
+        var deliveryCharge = account.GetDeliveryCharge();
+
+        // Assert
+        Assert.Equal(0m, deliveryCharge);
+    }
+
+    [Fact]
+    public void 消費税は会計アイテムの合計金額と送料に対してかかる()
+    {
+        // Arrange
+        var accountItems = new AccountItem[] { new(1, 4500m) };
+        var account = new Account(accountItems);
+
+        // Act
+        var tax = account.GetConsumptionTax();
+
+        // Assert
+        Assert.Equal(500m, tax);
+    }
+
+    [Fact]
+    public void 消費税は0円未満の端数が切り捨てられる()
+    {
+        // Arrange
+        var accountItems = new AccountItem[] { new(1, 19m) };
+        var account = new Account(accountItems);
+
+        // Act
+        var tax = account.GetConsumptionTax();
+
+        // Assert
+        Assert.Equal(51m, tax);
+    }
+
+    [Fact]
+    public void 税込み合計は会計アイテムの合計金額と送料と消費税額の合計となる()
+    {
+        // Arrange
+        var accountItems = new AccountItem[] { new(1, 4500m) };
+        var account = new Account(accountItems);
+
+        // Act
+        var totalPrice = account.GetTotalPrice();
+
+        // Assert
+        Assert.Equal(5500m, totalPrice);
+    }
+}
