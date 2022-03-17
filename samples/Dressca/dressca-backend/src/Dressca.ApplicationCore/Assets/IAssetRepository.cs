dd0bce202ef27d3284676355f00f9b27b58435d3
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-17 23:12:44 +0900

    アセットにアクセスするリポジトリとストアのインターフェースを追加

---
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Assets/IAssetRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Dressca.ApplicationCore.Assets;
+
+/// <summary>
+///  アセットリポジトリ。
+/// </summary>
+public interface IAssetRepository
+{
+    /// <summary>
+    ///  指定したアセットコードの情報を取得します。
+    ///  存在しない場合は <see langword="null"/> を返します。
+    /// </summary>
+    /// <param name="assetCode">アセットコード。</param>
+    /// <param name="cancellationToken">キャンセルトークン。</param>
+    /// <returns>アセット情報。</returns>
+    Asset? Find(string? assetCode, CancellationToken cancellationToken = default);
+}
---
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Assets/IAssetStore.cs
@@ -0,0 +1,18 @@
+﻿namespace Dressca.ApplicationCore.Assets;
+
+/// <summary>
+///  アセットのストア。
+/// </summary>
+public interface IAssetStore
+{
+    /// <summary>
+    ///  指定したアセット情報のストリームを取得します。
+    ///  見つからない場合は <see langword="null"/> を返します。
+    /// </summary>
+    /// <param name="asset">アセット情報。</param>
+    /// <returns>ストリーム。見つからない場合は <see langword="null"/> 。</returns>
+    /// <exception cref="ArgumentNullException">
+    ///  <paramref name="asset"/> が <see langword="null"/> です。
+    /// </exception>
+    Stream? GetStreamAsync(Asset asset);
+}
