cef814ab4772aad37c15c07b77a228bdeb64ddbe
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-03-18 23:37:26 +0900

    アセットの拡張メソッドの単体テストを追加

---
+++ samples/Dressca/dressca-backend/tests/Dressca.UnitTests/Web/Assets/AssetExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using Dressca.ApplicationCore.Assets;
+using Dressca.Web.Assets;
+
+namespace Dressca.UnitTests.Web.Assets;
+
+public class AssetExtensionsTest
+{
+    [Fact]
+    public void アセットがnullの場合例外()
+    {
+        // Arrange
+        Asset? asset = null;
+
+        // Act
+        var action = () => AssetExtensions.GetContentType(asset!);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>("asset", action);
+    }
+
+    [Fact]
+    public void アセットタイプがPNGの場合()
+    {
+        // Arrange
+        Asset asset = new Asset("asset-code", AssetTypes.Png);
+
+        // Act
+        var contentType = AssetExtensions.GetContentType(asset);
+
+        // Assert
+        Assert.Equal("image/png", contentType);
+    }
+}
