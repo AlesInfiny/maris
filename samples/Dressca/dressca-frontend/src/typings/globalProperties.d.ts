37079b55a7abb60e101ee39e4f2cd234a9d07034
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-04-15 15:54:44 +0900

    Revert "日本円表示への変換ロジックをGlobalFiltersに追加"
    This reverts commit 92de57d7b0218b7701a03ea6aff1afb70e57569d.

--- samples/Dressca/dressca-frontend/src/typings/globalProperties.d.ts
+++
@@ -1,7 +0,0 @@
-import type { GlobalFilters } from '@/shared/filters/globalFilters';
-
-declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
-    $filters: GlobalFilters;
-  }
-}
