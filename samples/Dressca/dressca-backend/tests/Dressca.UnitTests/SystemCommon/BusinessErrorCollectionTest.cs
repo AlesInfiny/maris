44ceaa2183f6457538345b30b1a4f986c10502eb
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-05-25 22:04:29 +0900

    業務エラーのコレクションを追加

---
+++ samples/Dressca/dressca-backend/tests/Dressca.UnitTests/SystemCommon/BusinessErrorCollectionTest.cs
@@ -0,0 +1,72 @@
+﻿using Dressca.SystemCommon;
+
+namespace Dressca.UnitTests.SystemCommon;
+
+public class BusinessErrorCollectionTest
+{
+    [Fact]
+    public void AddOrMerge_nullを追加しようとすると例外()
+    {
+        // Arrange
+        var errors = new BusinessErrorCollection();
+        BusinessError? newBusinessError = null;
+
+        // Act
+        var action = () => errors.AddOrMerge(newBusinessError!);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>("newBusinessError", action);
+    }
+
+    [Fact]
+    public void AddOrMerge_コレクションに追加していないエラーコードのデータは追加される()
+    {
+        // Arrange
+        var errors = new BusinessErrorCollection();
+        string errorCode = "ERR_CODE";
+        string errorMessage = "ERROR_MESSAGE";
+        BusinessError newBusinessError = new BusinessError(errorCode, errorMessage);
+
+        // Act
+        errors.AddOrMerge(newBusinessError);
+
+        // Assert
+        Assert.Collection(
+            errors,
+            error =>
+            {
+                Assert.Equal(errorCode, error.ErrorCode);
+                Assert.Collection(
+                    error.ErrorMessages,
+                    message => Assert.Equal(errorMessage, message));
+            });
+    }
+
+    [Fact]
+    public void AddOrMerge_コレクションに追加済みのエラーコードのデータはマージされる()
+    {
+        // Arrange
+        var errors = new BusinessErrorCollection();
+        string errorCode = "ERR_CODE";
+        string errorMessage1 = "ERROR_MESSAGE1";
+        string errorMessage2 = "ERROR_MESSAGE2";
+        BusinessError businessError1 = new BusinessError(errorCode, errorMessage1);
+        errors.AddOrMerge(businessError1);
+        BusinessError businessError2 = new BusinessError(errorCode, errorMessage2);
+
+        // Act
+        errors.AddOrMerge(businessError2);
+
+        // Assert
+        Assert.Collection(
+            errors,
+            error =>
+            {
+                Assert.Equal(errorCode, error.ErrorCode);
+                Assert.Collection(
+                    error.ErrorMessages,
+                    message => Assert.Equal(errorMessage1, message),
+                    message => Assert.Equal(errorMessage2, message));
+            });
+    }
+}
