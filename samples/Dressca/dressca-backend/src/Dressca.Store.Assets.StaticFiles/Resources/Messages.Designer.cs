3df013542fe0890c07980212fe53dc1867249eb4
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2022-05-31 13:51:07 +0900

    Store.Assets.StaticFilesのリソース名を修正

--- samples/Dressca/dressca-backend/src/Dressca.Store.Assets.StaticFiles/Resources/Messages.Designer.cs
+++ samples/Dressca/dressca-backend/src/Dressca.Store.Assets.StaticFiles/Resources/Messages.Designer.cs
@@ -22,14 +22,14 @@ namespace Dressca.Store.Assets.StaticFiles.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class StoreAssetsStaticFilesMessages {
+    internal class Messages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal StoreAssetsStaticFilesMessages() {
+        internal Messages() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Dressca.Store.Assets.StaticFiles.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Dressca.Store.Assets.StaticFiles.Resources.StoreAssetsStaticFilesMessages", typeof(StoreAssetsStaticFilesMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Dressca.Store.Assets.StaticFiles.Resources.Messages", typeof(Messages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
