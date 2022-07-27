8c88e441fe6916ee933c6cd9cbb3c1bc232b3cc9
yawano <yoshitaka.awano@biprogy.com>
2022-07-27 10:09:13 +0900

    プルリクエスト修正対応

--- samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Baskets/Basket.cs
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/Baskets/Basket.cs
@@ -90,11 +90,11 @@ public class Basket
     }
 
     /// <summary>
-    /// 買い物かごアイテムが空かどうか示す値を取得します。
+    ///  買い物かごアイテムが空かどうか示す値を取得します。
     /// </summary>
     /// <returns>買い物かごアイテムが空の場合は <see langword="true"/> 、そうでない場合は <see langword="false"/> 。</returns>
     public bool IsEmpty()
     {
-        return !Items.Any();
+        return !this.Items.Any();
     }
 }
