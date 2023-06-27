83a4b0aba84ba2f34077f373b4f14e2766473386
犬井太一 <taichi.inui@unisys.co.jp>
2023-06-27 14:33:07 +0900

    remove mount

--- samples/Dressca/dressca-frontend/src/components/__tests__/common/CarouselSlider.spec.ts
+++ samples/Dressca/dressca-frontend/src/components/__tests__/common/CarouselSlider.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { shallowMount, mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import CarouselSlider from '@/components/common/CarouselSlider.vue';
 
 const items = [
