d6d41ec37cede56c9965c39d647919fc30847190
犬井太一 <taichi.inui@unisys.co.jp>
2023-06-27 13:01:29 +0900

    ユニットテストを追加

--- samples/Dressca/dressca-frontend/src/components/common/CarouselSlider.vue
+++ samples/Dressca/dressca-frontend/src/components/common/CarouselSlider.vue
@@ -14,6 +14,7 @@ const props = defineProps<{
 const MOVE_THRESHOLD = 30;
 
 const state = reactive({
+  hasItems: true,
   currentIndex: 0,
   startX: null as number | null,
   diffX: 0,
@@ -21,7 +22,7 @@ const state = reactive({
 
 const nextSlide = () => {
   state.currentIndex =
-    (state.currentIndex + 1 + props.items.length) % props.items.length;
+    (state.currentIndex + 1 + props.items?.length) % props.items.length;
 };
 
 const prevSlide = () => {
@@ -64,6 +65,10 @@ const onTouchStart = (event: MouseEvent | TouchEvent) => {
 };
 
 onMounted(() => {
+  if (props.items.length == 0) {
+    state.hasItems = false;
+    return;
+  }
   window.addEventListener('mousemove', onTouchMove);
   window.addEventListener('touchmove', onTouchMove);
   window.addEventListener('mouseup', onTouchEnd);
@@ -79,45 +84,51 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="container">
-    <div
-      class="flex justify-center items-center"
-      @touchstart="onTouchStart"
-      @mousedown="onTouchStart"
-    >
-      <ChevronLeftIcon
-        class="h-10 w-10 text-gray-300 hover:text-gray-500"
-        @click="prevSlide"
-      />
-      <template v-for="index in props.items.length">
-        <template v-if="index == state.currentIndex + 1">
-          <div
-            :key="index"
-            :style="{
-              transform: `translate3d(${state.diffX}px, 0, 0)`,
-            }"
-          >
-            <slot v-bind="{ item: getItem(state.currentIndex) }"></slot>
-          </div>
+  <template v-if="state.hasItems">
+    <div data-test="body" class="container">
+      <div
+        class="flex justify-center items-center"
+        @touchstart="onTouchStart"
+        @mousedown="onTouchStart"
+      >
+        <ChevronLeftIcon
+          class="h-10 w-10 text-gray-300 hover:text-gray-500"
+          data-test="left-arrow"
+          @click="prevSlide"
+        />
+        <template v-for="index in props.items.length">
+          <template v-if="index == state.currentIndex + 1">
+            <div
+              :key="index"
+              data-test="slider"
+              :style="{
+                transform: `translate3d(${state.diffX}px, 0, 0)`,
+              }"
+            >
+              <slot v-bind="{ item: getItem(state.currentIndex) }"></slot>
+            </div>
+          </template>
         </template>
-      </template>
-      <ChevronRightIcon
-        class="h-10 w-10 text-gray-300 hover:text-gray-500"
-        @click="nextSlide"
-      />
+        <ChevronRightIcon
+          class="h-10 w-10 text-gray-300 hover:text-gray-500"
+          data-test="right-arrow"
+          @click="nextSlide"
+        />
+      </div>
+      <div class="flex justify-center">
+        <template v-for="index in props.items.length" :key="index">
+          <template v-if="index == state.currentIndex + 1">
+            <MinusSmIcon class="h-10 w-10 text-gray-500"></MinusSmIcon>
+          </template>
+          <template v-if="index != state.currentIndex + 1">
+            <MinusSmIcon
+              class="h-10 w-10 text-gray-300"
+              data-test="page-indicator"
+              @click="selectSlide(index - 1)"
+            ></MinusSmIcon>
+          </template>
+        </template>
+      </div>
     </div>
-    <div class="flex justify-center">
-      <template v-for="index in props.items.length" :key="index">
-        <template v-if="index == state.currentIndex + 1">
-          <MinusSmIcon class="h-10 w-10 text-gray-500"></MinusSmIcon>
-        </template>
-        <template v-if="index != state.currentIndex + 1">
-          <MinusSmIcon
-            class="h-10 w-10 text-gray-300"
-            @click="selectSlide(index - 1)"
-          ></MinusSmIcon>
-        </template>
-      </template>
-    </div>
-  </div>
+  </template>
 </template>
