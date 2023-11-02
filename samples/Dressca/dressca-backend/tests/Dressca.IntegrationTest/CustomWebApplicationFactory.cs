178882ce4630cf3b06242d48570454e90b2f0956
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2023-11-02 17:55:37 +0900

    接続文字列の修正。

---
+++ samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Common;
+using Dressca.EfInfrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dressca.IntegrationTest;
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<DresscaDbContext>));
+
+            services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbConnection));
+
+            services.Remove(dbConnectionDescriptor);
+
+            services.AddDbContext<DresscaDbContext>( options =>
+            {
+                options.UseSqlServer(@"Server=localhost,1433;Database=Dressca.Eshop;User=sa;Password=Passw0rd;");
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
