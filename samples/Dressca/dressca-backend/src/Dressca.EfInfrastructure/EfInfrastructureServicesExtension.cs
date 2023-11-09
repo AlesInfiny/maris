902d0ae4b8468484bc6a828992213411a6a654c0
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2023-11-09 18:17:32 +0900

    環境変数が有効か確認。

--- samples/Dressca/dressca-backend/src/Dressca.EfInfrastructure/EfInfrastructureServicesExtension.cs
+++ samples/Dressca/dressca-backend/src/Dressca.EfInfrastructure/EfInfrastructureServicesExtension.cs
@@ -50,6 +50,7 @@ public static class EfInfrastructureServicesExtension
         }
 
         // DbContext
+        // 結合テスト実行時に設定を差し替えるため、サービス登録をいったん削除してから登録
         services.RemoveAll<DbContextOptions<DresscaDbContext>>();
         services.AddDbContext<DresscaDbContext>(option =>
         {
--- samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/CustomWebApplicationFactory.cs
+++ samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/CustomWebApplicationFactory.cs
@@ -1,11 +1,7 @@
-﻿using System.Data.Common;
-using Dressca.EfInfrastructure;
-using Microsoft.AspNetCore.Builder;
+﻿using Dressca.EfInfrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Dressca.IntegrationTest;
 public class CustomWebApplicationFactory<TProgram>
@@ -13,17 +9,27 @@ public class CustomWebApplicationFactory<TProgram>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+        //var webhostenv = builder.GetSetting("environment");
+        //builder.UseEnvironment("Development");
+
         builder.ConfigureServices(services =>
         {
-            var fileDirectory = Environment.GetEnvironmentVariable("appSettingsFileDirectory");
+            //var filePath = Environment.GetEnvironmentVariable("appSettingsFilePath");
+            //var config = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile(filePath, optional: true, reloadOnChange: true)
+            //    .Build();
+
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile(fileDirectory, optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
                 .Build();
 
             services.AddDresscaEfInfrastructure(config);
         });
 
-        builder.UseEnvironment("Development");
     }
 }
