27d58fc2178309ddffb4675fbc564e2e891babd8
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2023-11-10 10:49:58 +0900

    環境変数名を修正し、不要な処理を削除。

--- samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/CustomWebApplicationFactory.cs
+++ samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/CustomWebApplicationFactory.cs
@@ -9,14 +9,13 @@ public class CustomWebApplicationFactory<TProgram>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+        var env = Environment.GetEnvironmentVariable("TEST_ENVIRONMENT") ?? "Development";
 
         builder.ConfigureServices(services =>
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
                 .Build();
 
             services.AddDresscaEfInfrastructure(config);
