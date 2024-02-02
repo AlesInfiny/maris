5628f6fb0fc8b90027662913e46a1fab0295d419
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-02-02 16:59:36 +0900

    XunitLoggingBuilderExtensionsを追加

---
+++ samples/Dressca/dressca-backend/src/Maris.Diagnostics.Testing/Xunit/XunitLoggingBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Maris.Diagnostics.Testing.Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.Logging;
+
+/// <summary>
+///  Xunit で使用可能な <see cref="ILogger"/> を登録する処理を提供します。
+/// </summary>
+public static class XunitLoggingBuilderExtensions
+{
+    /// <summary>
+    ///  <paramref name="builder"/> に Xunit の <see cref="ILoggerProvider"/> を追加します。
+    /// </summary>
+    /// <param name="builder">Xunit の <see cref="ILoggerProvider"/> 追加する <see cref="ILoggingBuilder"/> 。</param>
+    /// <param name="loggerManager">Xunit で利用できる <see cref="ILogger"/> を管理する <see cref="TestLoggerManager"/> 。</param>
+    /// <returns><see cref="ILoggingBuilder"/> 。</returns>
+    /// <exception cref="ArgumentNullException">
+    ///  <list type="bullet">
+    ///   <item><paramref name="builder"/> が <see langword="null"/> です。</item>
+    ///   <item><paramref name="loggerManager"/> が <see langword="null"/> です。</item>
+    ///  </list>
+    /// </exception>
+    public static ILoggingBuilder AddXunitLogging(this ILoggingBuilder builder, TestLoggerManager loggerManager)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(loggerManager);
+        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, XunitLoggerProvider>(
+            provider => loggerManager.XunitLoggerProvider));
+        return builder;
+    }
+}
