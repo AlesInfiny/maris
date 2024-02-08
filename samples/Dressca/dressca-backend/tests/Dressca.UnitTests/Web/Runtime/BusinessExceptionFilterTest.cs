2f9574c38b94e056739ccbc7d7940d377f532f02
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-02-08 15:35:15 +0900

    不要なusingを削除

--- samples/Dressca/dressca-backend/tests/Dressca.UnitTests/Web/Runtime/BusinessExceptionFilterTest.cs
+++ samples/Dressca/dressca-backend/tests/Dressca.UnitTests/Web/Runtime/BusinessExceptionFilterTest.cs
@@ -1,6 +1,5 @@
 ﻿using Dressca.SystemCommon;
 using Dressca.Web.Runtime;
-using Maris.Logging.Testing.Xunit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
