9f20e6a7e34953a44be9b3be66f3a61a22ba4ab3
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2024-02-13 17:09:46 +0900

    テストしやすいようにConsoleAppContextの生成処理を切り出し

--- samples/ConsoleAppWithDI/solution/src/Maris.ConsoleApp.Hosting/ServiceCollectionExtensions.cs
+++ samples/ConsoleAppWithDI/solution/src/Maris.ConsoleApp.Hosting/ServiceCollectionExtensions.cs
@@ -76,35 +76,63 @@ public static class ServiceCollectionExtensions
     /// <returns>サービスを追加済みのサービスコレクション。</returns>
     internal static IServiceCollection AddConsoleAppContext(this IServiceCollection services, IEnumerable<string> args, Action<CommandParameterTypeCollection>? commandParametersOption = null)
         => services.AddSingleton(provider =>
-            {
-                var logger = provider.GetService<ILoggerFactory>()?.CreateLogger(typeof(ServiceCollectionExtensions));
-                logger?.LogInformation(Messages.ParseParameter.Embed(string.Join(' ', args)));
+        {
+            var logger = provider.GetService<ILoggerFactory>()?.CreateLogger(typeof(ServiceCollectionExtensions));
+            var settings = provider.GetRequiredService<ConsoleAppSettings>();
+            var appProcess = provider.GetRequiredService<IApplicationProcess>();
+            return CreateConsoleAppContext(args, commandParametersOption, appProcess, settings, logger);
+        });
 
-                var settings = provider.GetRequiredService<ConsoleAppSettings>();
-                var appProcess = provider.GetRequiredService<IApplicationProcess>();
-                var commandParameterTypes = new CommandParameterTypeCollection();
-                if (commandParametersOption is null)
-                {
-                    commandParameterTypes.InitializeFromAllAssemblies();
-                }
-                else
-                {
-                    commandParametersOption(commandParameterTypes);
-                }
+    /// <summary>
+    ///  コンソールアプリケーションの実行コンテキストを生成します。
+    ///  単体テスト用に公開しています。
+    /// </summary>
+    /// <param name="args">コンソールアプリケーションの起動引数。</param>
+    /// <param name="commandParametersOption">
+    ///  コマンドの名前とコマンドの型を管理するコレクションのオプション設定を実行します。
+    ///  既定値は <see langword="null"/> です。
+    /// </param>
+    /// <param name="appProcess">アプリケーションのプロセスを表すインターフェース。</param>
+    /// <param name="settings">コンソールアプリケーションの設定。</param>
+    /// <param name="logger">ロガー。</param>
+    /// <returns>生成した <see cref="CreateConsoleAppContext"/> 。</returns>
+    /// <exception cref="InvalidOperationException">
+    ///  <list type="bullet">
+    ///   <item>コマンドパラメーターの型が見つかりません。</item>
+    ///  </list>
+    /// </exception>
+    internal static ConsoleAppContext CreateConsoleAppContext(
+        IEnumerable<string> args,
+        Action<CommandParameterTypeCollection>? commandParametersOption,
+        IApplicationProcess appProcess,
+        ConsoleAppSettings settings,
+        ILogger? logger)
+    {
+        logger?.LogInformation(Messages.ParseParameter.Embed(string.Join(' ', args)));
 
-                if (!commandParameterTypes.Any())
-                {
-                    var assemblies = string.Join(',', commandParameterTypes.LoadedAssemblies.Select(asm => asm.GetName().Name));
-                    throw new InvalidOperationException(
-                        Messages.CommandParameterIsNotExists.Embed(typeof(CommandAttribute), assemblies));
-                }
+        var commandParameterTypes = new CommandParameterTypeCollection();
+        if (commandParametersOption is null)
+        {
+            commandParameterTypes.InitializeFromAllAssemblies();
+        }
+        else
+        {
+            commandParametersOption(commandParameterTypes);
+        }
 
-                var param = Parser.Default.ParseArguments(args, commandParameterTypes.ToArray());
-                if (param is null || param.Tag == ParserResultType.NotParsed)
-                {
-                    appProcess.Exit(settings.DefaultValidationErrorExitCode);
-                }
+        if (!commandParameterTypes.Any())
+        {
+            var assemblies = string.Join(',', commandParameterTypes.LoadedAssemblies.Select(asm => asm.GetName().Name));
+            throw new InvalidOperationException(
+                Messages.CommandParameterIsNotExists.Embed(typeof(CommandAttribute), assemblies));
+        }
 
-                return new ConsoleAppContext(param.Value);
-            });
+        var param = Parser.Default.ParseArguments(args, commandParameterTypes.ToArray());
+        if (param is null || param.Tag == ParserResultType.NotParsed)
+        {
+            appProcess.Exit(settings.DefaultValidationErrorExitCode);
+        }
+
+        return new ConsoleAppContext(param.Value);
+    }
 }
