b1bec26ec2cdd11d10653f9fb577280c71e86767
犬井太一 <taichi.inui@unisys.co.jp>
2024-02-20 16:51:54 +0900

    環境変数名にVITE_を追加

--- samples/Dressca/dressca-frontend/env.d.ts
+++ samples/Dressca/dressca-frontend/env.d.ts
@@ -2,7 +2,7 @@
 interface ImportMetaEnv {
   readonly VITE_NO_ASSET_URL: string;
   readonly VITE_ASSET_URL: string;
-  readonly AXIOS_BASE_ENDPOINT_ORIGIN: string;
+  readonly VITE_AXIOS_BASE_ENDPOINT_ORIGIN: string;
   readonly VITE_PROXY_ENDPOINT_ORIGIN: string;
   readonly VITE_API_PREFIX: string;
 }
