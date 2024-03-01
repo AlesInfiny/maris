1fad23a583a0190ae64477240fc9f0b7f8ad9537
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2024-03-01 10:18:12 +0900

    Revert "認証サンプル用のフォルダーを追加"
    This reverts commit d2f5fb5a747751b64d37d2ce293fef388e5e15c8.

--- samples/AuthenticationSample/AzureADB2C/dressca-frontend/src/validation/validation-items.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-// バリデーション定義（一元化）
-export const validationItems = {
-  email: yup.string().email(),
-};
--- samples/AuthenticationSample/AzureADB2C/dressca-frontend/src/views/error/ErrorView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>エラーが発生しました。</h1>
-  </div>
-</template>
