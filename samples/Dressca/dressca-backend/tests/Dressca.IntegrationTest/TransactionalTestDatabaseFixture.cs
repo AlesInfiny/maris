9e2aadc0a4f7d89c430ee77a1e3d1adf8d417559
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-03-04 13:37:01 +0900

    EFにおける値オブジェクトの扱いを所有型から複合型に変更する

---
+++ samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/TransactionalTestDatabaseFixture.cs
@@ -0,0 +1,40 @@
+﻿using Dressca.EfInfrastructure;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Dressca.IntegrationTest;
+
+public class TransactionalTestDatabaseFixture
+{
+    private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=Dressca.Eshop.IT.;Integrated Security=True;";
+
+    public TransactionalTestDatabaseFixture()
+    {
+        using (var context = this.CreateContext())
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            this.Cleanup();
+        }
+    }
+
+    public void Cleanup()
+    {
+        using var context = this.CreateContext();
+
+        context.Orders.RemoveRange(context.Orders);
+        context.SaveChanges();
+    }
+
+    internal DresscaDbContext CreateContext()
+
+        => new DresscaDbContext(
+            new DbContextOptionsBuilder<DresscaDbContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+
+    [CollectionDefinition("TransactionalTests")]
+    public class TransactionalTestsCollection : ICollectionFixture<TransactionalTestDatabaseFixture>
+    {
+    }
+}
