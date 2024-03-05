7c5fce0a6d6ebed5e5635b9469a25fe2fc3b0bcc
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-03-05 10:31:28 +0900

    パイプライン上で動作するように接続文字列を切り替えるよう修正

--- samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/TransactionalTestDatabaseFixture.cs
+++ samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/TransactionalTestDatabaseFixture.cs
@@ -1,15 +1,19 @@
 ﻿using Dressca.EfInfrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Xunit;
 
 namespace Dressca.IntegrationTest;
 
 public class TransactionalTestDatabaseFixture
 {
-    private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=Dressca.Eshop.IT;Integrated Security=True;";
+    private string connectionString;
 
     public TransactionalTestDatabaseFixture()
     {
+        this.connectionString = this.GetConnectionString();
+
         using (var context = this.CreateContext())
         {
             context.Database.EnsureDeleted();
@@ -25,11 +29,30 @@ public class TransactionalTestDatabaseFixture
         context.SaveChanges();
     }
 
+    public string GetConnectionString()
+    {
+        var env = Environment.GetEnvironmentVariable("TEST_ENVIRONMENT") ?? Environments.Development;
+
+        if (!env.Equals(Environments.Development, StringComparison.OrdinalIgnoreCase))
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+                .Build();
+            return config.GetConnectionString("DresscaTransactionalDbContext") ?? throw new ArgumentNullException("DresscaTransactionalDbContext");
+        }
+        else
+        {
+            const string localConnectionString = @"Server=(localdb)\mssqllocaldb;Database=Dressca.Eshop.IT;Integrated Security=True;";
+            return localConnectionString;
+        }
+    }
+
     internal DresscaDbContext CreateContext()
-        => new DresscaDbContext(
-            new DbContextOptionsBuilder<DresscaDbContext>()
-                .UseSqlServer(ConnectionString)
-                .Options);
+            => new DresscaDbContext(
+                new DbContextOptionsBuilder<DresscaDbContext>()
+                    .UseSqlServer(this.connectionString)
+                    .Options);
 
     [CollectionDefinition("TransactionalTests")]
     public class TransactionalTestsCollection : ICollectionFixture<TransactionalTestDatabaseFixture>
