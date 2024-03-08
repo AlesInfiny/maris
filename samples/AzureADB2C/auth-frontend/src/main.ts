a4898c48e4ec1ffe93f8618e7bb99e7231b7d525
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2024-03-08 14:31:19 +0900

    CIパイプラインを修正

--- samples/AzureADB2C/auth-frontend/src/main.ts
+++ samples/AzureADB2C/auth-frontend/src/main.ts
@@ -2,12 +2,9 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
-import { msalInstance } from '@/shared/authentication/authentication-config';
-
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
-app.use(msalInstance);
 
 app.mount('#app');
