96dbe37f00f7ce2343ef87ae1ea88d62ea6bad5a
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-03-13 10:00:56 +0900

    結合テストスイートをリファクタリングする

--- samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/ShoppingTest.cs
+++ samples/Dressca/dressca-backend/tests/Dressca.IntegrationTest/ShoppingTest.cs
@@ -10,6 +10,7 @@ namespace Dressca.IntegrationTest;
 public class ShoppingTest(IntegrationTestWebApplicationFactory<Program> factory)
     : IClassFixture<IntegrationTestWebApplicationFactory<Program>>
 {
+    private static readonly JsonSerializerOptions JsonSerializerWebOptions = new(JsonSerializerDefaults.Web);
     private readonly IntegrationTestWebApplicationFactory<Program> factory = factory;
     private readonly HttpClient client = factory.CreateClient();
 
@@ -24,53 +25,69 @@ public class ShoppingTest(IntegrationTestWebApplicationFactory<Program> factory)
         // Act
         var postBasketItemResponse = await this.client.PostAsJsonAsync("api/basket-items", postBasketItemsRequest);
         postBasketItemResponse.EnsureSuccessStatusCode();
-        var cookies = postBasketItemResponse.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
 
+        // API側でBuyerIdを特定できるように、Cookieを付加してリクエストする
+        var cookies = postBasketItemResponse.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
+        var postOrderResponse = await this.PostOrder(postOrderRequestJson, cookies);
+        var getOrderResponse = await this.GetOrder(cookies, postOrderResponse);
+        var orderResponse = await this.DeserializeOrderResponse(getOrderResponse);
+
+        // Assert
+        Assert.NotNull(orderResponse);
+        var orderItemResponse = Assert.Single(orderResponse.OrderItems);
+        Assert.NotNull(orderItemResponse.ItemOrdered);
+        Assert.Equal(postBasketItemsRequest.AddedQuantity, orderItemResponse.Quantity);
+        Assert.Equal(postBasketItemsRequest.CatalogItemId, orderItemResponse.ItemOrdered.Id);
+    }
+
+    private async Task<OrderResponse?> DeserializeOrderResponse(HttpResponseMessage getOrderResponse)
+    {
+        var orderResponseJson = await getOrderResponse.Content.ReadAsStringAsync();
+        var orderResponse = JsonSerializer.Deserialize<OrderResponse>(orderResponseJson, JsonSerializerWebOptions);
+        return orderResponse;
+    }
+
+    private async Task<HttpResponseMessage> GetOrder(IEnumerable<string> cookies, HttpResponseMessage postOrderResponse)
+    {
+        var getOrderRequest = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = postOrderResponse.Headers.Location,
+        };
+
+        getOrderRequest.Headers.Add("Cookie", cookies);
+        var getOrderResponse = await this.client.SendAsync(getOrderRequest);
+        getOrderResponse.EnsureSuccessStatusCode();
+        return getOrderResponse;
+    }
+
+    private async Task<HttpResponseMessage> PostOrder(string postOrderRequestJson, IEnumerable<string> cookies)
+    {
         var postOrderRequest = new HttpRequestMessage
         {
             Content = new StringContent(postOrderRequestJson, Encoding.UTF8, "application/json"),
             Method = HttpMethod.Post,
             RequestUri = new Uri("api/orders", UriKind.Relative),
         };
+
         postOrderRequest.Headers.Add("Cookie", cookies);
         var postOrderResponse = await this.client.SendAsync(postOrderRequest);
         postOrderResponse.EnsureSuccessStatusCode();
-
-        var getOrderRequest = new HttpRequestMessage
-        {
-            Method = HttpMethod.Get,
-            RequestUri = postOrderResponse.Headers.Location,
-        };
-        getOrderRequest.Headers.Add("Cookie", cookies);
-        var result = await this.client.SendAsync(getOrderRequest);
-        result.EnsureSuccessStatusCode();
-
-        var orderResponseJson = await result.Content.ReadAsStringAsync();
-        var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
-        var orderResponse = JsonSerializer.Deserialize<OrderResponse>(orderResponseJson, options);
-
-        // Assert
-        Assert.NotNull(orderResponse);
-        var orderItemResponse = orderResponse.OrderItems.FirstOrDefault();
-        Assert.NotNull(orderItemResponse);
-        Assert.NotNull(orderItemResponse.ItemOrdered);
-        Assert.Equal(postBasketItemsRequest.AddedQuantity, orderItemResponse.Quantity);
-        Assert.Equal(postBasketItemsRequest.CatalogItemId, orderItemResponse.ItemOrdered.Id);
+        return postOrderResponse;
     }
 
     private PostBasketItemsRequest CreateBasketItemsRequest()
     {
-        var postBasketItemsRequest = new PostBasketItemsRequest()
+        return new()
         {
             CatalogItemId = 1,
             AddedQuantity = 2,
         };
-        return postBasketItemsRequest;
     }
 
     private PostOrderRequest CreateDefaultPostOrderRequest()
     {
-        var postOrderRequest = new PostOrderRequest()
+        return new()
         {
             FullName = "国会　太郎",
             PostalCode = "100-8924",
@@ -78,7 +95,5 @@ public class ShoppingTest(IntegrationTestWebApplicationFactory<Program> factory)
             Shikuchoson = "千代田区",
             AzanaAndOthers = "永田町1-10-1",
         };
-
-        return postOrderRequest;
     }
 }
