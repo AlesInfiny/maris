651d62a7775ad9fcbbd47ff77433fdf1515e0873
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2024-03-14 14:12:41 +0900

    Dressca.Store.Assets.StaticFilesのメッセージ用リソースファイルをLogMessage.resxをリネーム。(既存のメッセージはすべてログ用であるため)

--- samples/Dressca/dressca-backend/src/Dressca.Store.Assets.StaticFiles/Resources/LogMessages.Designer.cs
+++ samples/Dressca/dressca-backend/src/Dressca.Store.Assets.StaticFiles/Resources/LogMessages.Designer.cs
@@ -22,14 +22,14 @@ namespace Dressca.Store.Assets.StaticFiles.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Messages {
+    internal class LogMessages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Messages() {
+        internal LogMessages() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Dressca.Store.Assets.StaticFiles.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Dressca.Store.Assets.StaticFiles.Resources.Messages", typeof(Messages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Dressca.Store.Assets.StaticFiles.Resources.LogMessages", typeof(LogMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
