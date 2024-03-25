d9ea444ac0f4fad4e275b342809afd010f3b3355
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2024-03-25 14:47:08 +0900

    lint のエラーに対応

--- samples/Dressca/dressca-frontend/cypress.config.ts
+++ samples/Dressca/dressca-frontend/cypress.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
+import { setupNodeEvents } from './cypress/plugins/index';
 
 export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
-    },
+    setupNodeEvents,
     baseUrl: 'http://localhost:5050',
   },
-})
+});
