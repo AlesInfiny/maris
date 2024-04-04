a84b409b8c8718f6bf4584121f192b8b453bca3a
犬井太一 <taichi.inui@unisys.co.jp>
2024-04-04 09:06:47 +0900

    fix lint

--- samples/Dressca/dressca-frontend/src/services/catalog/catalog-service.ts
+++ samples/Dressca/dressca-frontend/src/services/catalog/catalog-service.ts
@@ -5,8 +5,8 @@ const catalogStore = useCatalogStore();
 export async function fetchCategoriesAndBrands() {
   catalogStore.fetchCategories();
   catalogStore.fetchBrands();
-};
+}
 
 export async function fetchItems(categoryId: number, brandsId: number) {
   catalogStore.fetchItems(categoryId, brandsId);
-};
+}
