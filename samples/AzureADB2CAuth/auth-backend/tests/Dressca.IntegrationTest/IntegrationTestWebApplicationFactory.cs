6f3f1eede87d1b6ad7c4de9b583d3b13a5f3aebe
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2024-04-12 14:25:45 +0900

    JWT認証処理を結合テストプロジェクトに追加。

---
+++ samples/AzureADB2CAuth/auth-backend/tests/Dressca.IntegrationTest/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,75 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Dressca.IntegrationTest;
+public class IntegrationTestWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram>
+    where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var config = GetConfiguration();
+
+            // JwtBearerの設定追加(option)
+            // AddJwtBearerでScheme名を変えれば名前衝突しないが、認証に利用されない
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                // appsettings.jsonから読み込む形に変える
+                options.TokenValidationParameters =
+                new TokenValidationParameters
+                {
+                    ValidIssuer = config["Jwt:Issuer"],
+                    ValidAudience = config["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey
+                        (Encoding.UTF8.GetBytes(config["Jwt:Key"] ?? throw new NullReferenceException("Jwt:Key"))),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+        });
+    }
+
+    public IConfiguration GetConfiguration()
+    {
+        // テスト用のappsettings.jsonを追加(Jwtの設定内容を記載)
+        return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+    }
+
+    public string CreateToken(string? key, string? issuer, string? audience, string userName, string email)
+    {
+        // JWTトークンの生成
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+                new Claim(JwtRegisteredClaimNames.Sub, userName),
+                new Claim(JwtRegisteredClaimNames.Email, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+        var token = new JwtSecurityToken(
+            issuer: issuer,
+            audience: audience,
+            claims: claims,
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ samples/AzureADB2CAuth/auth-backend/tests/Dressca.IntegrationTest/WebApiTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using Xunit;
+
+namespace Dressca.IntegrationTest;
+
+public class WebApiTest(IntegrationTestWebApplicationFactory<Program> factory)
+    : IClassFixture<IntegrationTestWebApplicationFactory<Program>>
+{
+    private readonly IntegrationTestWebApplicationFactory<Program> factory = factory;
+
+    [Fact]
+    public async Task Get_ApiAuth_認証エラー_Status401を返す()
+    {
+        // Arrange
+        var client = this.factory.CreateClient();
+
+        // Act
+        // 認証せずにリクエスト送信
+        var response = await client.GetAsync("api/users");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Get_ApiAuth_認証成功_UserIDを返す()
+    {
+        // Arrange
+        var client = this.factory.CreateClient();
+        // 取得したJWTをヘッダーに追加
+        var config = factory.GetConfiguration();
+        var token = this.factory.CreateToken(config["Jwt:Key"], config["Jwt:Issuer"], config["Jwt:Audience"], "testUser", "email");
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        // Act
+        var response = await client.GetAsync("api/users");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.Equal("{\"userId\":\"testUser\"}", result);
+    }
+
+    [Fact]
+    public async Task Get_認証不要なAPI_サーバー時間を返す()
+    {
+        // Arrange
+        var client = this.factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("api/servertime");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadAsStringAsync();
+        // "{"serverTime":"2024/04/12 13:32:59"}"
+        Assert.StartsWith("{\"serverTime\":", result);
+    }
+}
