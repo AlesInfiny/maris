67561bf9fb469d48c9a83d1dac8940cfc83f4aa0
犬井太一 <taichi.inui@unisys.co.jp>
2024-04-18 11:21:44 +0900

    useStoreをメソッド内に移動

--- samples/AzureADB2CAuth/auth-frontend/src/services/authentication/authentication-service.ts
+++ samples/AzureADB2CAuth/auth-frontend/src/services/authentication/authentication-service.ts
@@ -1,11 +1,10 @@
 import { useAuthenticationStore } from '@/stores/authentication/authentication';
 import { useUserStore } from '@/stores/user/user';
 
-const authenticationStore = useAuthenticationStore();
-const userStore = useUserStore();
-
 export const authenticationService = {
   async signIn() {
+    const authenticationStore = useAuthenticationStore();
+    const userStore = useUserStore();
     await authenticationStore.signIn();
     if (authenticationStore.isAuthenticated) {
       await userStore.fetchUserResponse();
@@ -13,6 +12,7 @@ export const authenticationService = {
   },
 
   async fetchToken() {
+    const authenticationStore = useAuthenticationStore();
     await authenticationStore.getToken();
   },
 };
