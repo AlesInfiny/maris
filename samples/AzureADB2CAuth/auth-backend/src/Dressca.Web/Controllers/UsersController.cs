7435b42991c3c1a780973597dbca3aa9e72a35cb
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2024-04-25 17:36:54 +0900

    Azure AD B2CサンプルでProducesResponseTypeの設定を修正。

--- samples/AzureADB2CAuth/auth-backend/src/Dressca.Web/Controllers/UsersController.cs
+++ samples/AzureADB2CAuth/auth-backend/src/Dressca.Web/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ public class UsersController : ControllerBase
     /// <response code="401">認証されていない。</response>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserResponse))]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ProblemDetails))]
     [Authorize]
     [OpenApiOperation("getUser")]
     public IActionResult GetUser()
