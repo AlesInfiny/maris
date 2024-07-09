ec0f1d55bcb71f94ead7a4cd16817911d760322d
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-07-09 10:23:17 +0900

    管理画面用のバックエンドプロジェクトを作成

--- samples/Dressca/dressca-frontend/apps/admin/vite.config.ts
+++ samples/Dressca/dressca-frontend/apps/admin/vite.config.ts
@@ -1,18 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const plugins = [vue(), vueJsx()];
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      port: 6173,
+      proxy: {
+        '/api': {
+          target: env.VITE_PROXY_ENDPOINT_ORIGIN,
+          changeOrigin: true,
+          autoRewrite: true,
+          secure: false,
+        },
+        '/swagger': {
+          target: env.VITE_PROXY_ENDPOINT_ORIGIN,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+});
