4df08d650e3db7509f11244e8a12ea3cd07da684
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2024-07-11 10:07:19 +0900

    Airbnb JavaScript Style Guideを適用するよう設定。

--- samples/Dressca/dressca-frontend/.eslintrc.cjs
+++ samples/Dressca/dressca-frontend/.eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-airbnb-with-typescript',
     '@vue/eslint-config-prettier',
   ],
   env: {
