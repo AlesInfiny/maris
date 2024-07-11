a1e445c69cb6333ed63e2843178fcdcad14e2f2e
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2024-07-11 10:10:55 +0900

    vue/setup-compiler-macrosの設定はvue-eslint-parser v9.0.0以降で不要になったので削除。

--- samples/Dressca/dressca-frontend/.eslintrc.cjs
+++ samples/Dressca/dressca-frontend/.eslintrc.cjs
@@ -9,9 +9,6 @@ module.exports = {
     '@vue/eslint-config-airbnb-with-typescript',
     '@vue/eslint-config-prettier',
   ],
-  env: {
-    'vue/setup-compiler-macros': true,
-  },
   overrides: [
     {
       files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
