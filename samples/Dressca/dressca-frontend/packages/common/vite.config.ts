af8046f493f665391edf1984f9559dfd1a9d9498
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-07-17 16:19:08 +0900

    型チェック順を制御する

--- samples/Dressca/dressca-frontend/packages/common/vite.config.ts
+++ samples/Dressca/dressca-frontend/packages/common/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'CommonLib',
-      fileName: 'common-lib'
+      fileName: 'common-lib',
     },
     rollupOptions: {
       external: ['vue'],
