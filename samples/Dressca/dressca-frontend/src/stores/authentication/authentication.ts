c60a30e37aa0abe7f4f72027f9713fff74a11e0a
犬井太一 <taichi.inui@unisys.co.jp>
2024-07-18 10:29:51 +0900

    認証情報の永続化に sessionStorage を利用

--- samples/Dressca/dressca-frontend/src/stores/authentication/authentication.ts
+++ samples/Dressca/dressca-frontend/src/stores/authentication/authentication.ts
@@ -3,11 +3,17 @@ import { defineStore } from 'pinia';
 export const useAuthenticationStore = defineStore({
   id: 'authentication',
   state: () => ({
-    _isAuthenticated: false,
+    _isAuthenticated: JSON.parse(
+      sessionStorage.getItem('isAuthenticated') || 'false',
+    ) as boolean,
   }),
   actions: {
     async signInAsync() {
       this._isAuthenticated = true;
+      sessionStorage.setItem(
+        'isAuthenticated',
+        JSON.stringify(this._isAuthenticated),
+      );
     },
   },
   getters: {
