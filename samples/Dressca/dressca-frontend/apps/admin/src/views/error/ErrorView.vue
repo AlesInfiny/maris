fe5c7527a8eac1cc434d30f46c529fe63b9a625d
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-08-06 11:05:58 +0900

    カラーを変更

--- samples/Dressca/dressca-frontend/apps/admin/src/views/error/ErrorView.vue
+++ samples/Dressca/dressca-frontend/apps/admin/src/views/error/ErrorView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="m-10 bg-red-100 border border-red-400 text-red-700 font-bold px-4 py-5 rounded"
+    class="m-10 bg-red-800 border border-red-900 text-gray-0 font-bold px-4 py-5 rounded"
   >
     エラーが発生しました。
   </div>
