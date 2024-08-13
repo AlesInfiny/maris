99a45a3ebe6a0b48c15feaeb3ed351e67c7175ac
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-08-13 14:56:56 +0900

    型チェックへの対処漏れを修正

--- samples/Dressca/dressca-frontend/packages/common/src/index.ts
+++ samples/Dressca/dressca-frontend/packages/common/src/index.ts
@@ -1,4 +1,4 @@
 import LoadingSpinner from './components/LoadingSpinner.vue';
-import currencyHelper from './helpers/currencyHelper';
+import { currencyHelper } from './helpers/currencyHelper';
 
 export { LoadingSpinner, currencyHelper };
