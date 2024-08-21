744189a58c2f7262965b64da94c0ba808b6275ef
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-08-21 15:22:53 +0900

    frontend配下のみをmerge用にrestoreしておく

---
+++ samples/Dressca/dressca-frontend/packages/common/src/index.ts
@@ -0,0 +1,4 @@
+import LoadingSpinner from './components/LoadingSpinner.vue';
+import { currencyHelper } from './helpers/currencyHelper';
+
+export { LoadingSpinner, currencyHelper };
---
+++ samples/Dressca/dressca-frontend/packages/common/vitest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-default-export */
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+);
