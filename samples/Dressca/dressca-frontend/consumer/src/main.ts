a6adb0430ae807722b6eddb486c031f64340c4f7
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-10-08 11:34:08 +0900

    エラーハンドラーを実装し、買い物かご画面で動作確認

--- samples/Dressca/dressca-frontend/consumer/src/main.ts
+++ samples/Dressca/dressca-frontend/consumer/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { authenticationGuard } from '@/shared/authentication/authentication-guard';
 import { errorHandlerPlugin } from '@/shared/error-handler/error-handler-plugin';
+import { createCustomErrorHandler } from './shared/error-handler/custom-error-handler';
 import App from './App.vue';
 import { router } from './router';
 
@@ -10,11 +11,13 @@ import '@/config/yup.config';
 
 const app = createApp(App);
 const pinia = createPinia();
+const customErrorHandler = createCustomErrorHandler();
 
 app.use(pinia);
 app.use(router);
 
 app.use(errorHandlerPlugin);
+app.use(customErrorHandler);
 
 authenticationGuard(router);
 
