bcfe1f801f826f77c0bf5d82d7ca00a889012e44
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-11-05 22:14:01 +0900

    [Dressca-Consumer]maia@0.9.1のフロントエンドのコードをmarisに移植する (#1961)
    * maia@0.9.1のコードを移植
    
    * openapi-generator@7.9.0でクライアントコードを再生成

--- samples/Dressca/dressca-frontend/consumer/src/services/basket/basket-service.ts
+++ samples/Dressca/dressca-frontend/consumer/src/services/basket/basket-service.ts
@@ -1,12 +1,12 @@
 import { useBasketStore } from '@/stores/basket/basket';
 
-const basketStore = useBasketStore();
-
 export async function fetchBasket() {
+  const basketStore = useBasketStore();
   await basketStore.fetch();
 }
 
 export async function addItemToBasket(itemId: number) {
+  const basketStore = useBasketStore();
   await basketStore.add(itemId);
   await basketStore.fetch();
 }
@@ -15,6 +15,7 @@ export async function updateItemInBasket(
   catalogItemId: number,
   newQuantity: number,
 ) {
+  const basketStore = useBasketStore();
   // 直前に追加された商品の表示を更新するためIDを削除
   basketStore.deleteAddedItemId();
 
@@ -26,6 +27,7 @@ export async function updateItemInBasket(
 }
 
 export async function removeItemFromBasket(catalogItemId: number) {
+  const basketStore = useBasketStore();
   // 直前に追加された商品の表示を更新するためIDを削除
   basketStore.deleteAddedItemId();
   try {
