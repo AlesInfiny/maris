a9e8a5226a5933c4a58e9aadb19f4a4715129f9d
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-11-13 16:25:32 +0900

    イベントIDの番号の並びが不自然に見える箇所を修正

--- samples/Dressca/dressca-backend/src/Dressca.Web.Consumer/Events.cs
+++ samples/Dressca/dressca-backend/src/Dressca.Web.Consumer/Events.cs
@@ -8,22 +8,22 @@ internal static class Events
     /// <summary>
     /// クライアント側から不正なHTTPリクエストを受信したことを示すイベントID
     /// </summary>
-    internal static readonly EventId ReceiveHttpBadRequest = new(1101, nameof(ReceiveHttpBadRequest));
+    internal static readonly EventId ReceiveHttpBadRequest = new(1001, nameof(ReceiveHttpBadRequest));
 
     /// <summary>
     /// アセットが見つからなかったことを示すイベントID
     /// </summary>
-    internal static readonly EventId AssetNotFound = new(1201, nameof(AssetNotFound));
+    internal static readonly EventId AssetNotFound = new(1101, nameof(AssetNotFound));
 
     /// <summary>
     /// 指定された商品が買い物かご内に存在しなかったことを示すイベントID
     /// </summary>
-    internal static readonly EventId CatalogItemIdDoesNotExistInBasket = new(1301, nameof(CatalogItemIdDoesNotExistInBasket));
+    internal static readonly EventId CatalogItemIdDoesNotExistInBasket = new(1201, nameof(CatalogItemIdDoesNotExistInBasket));
 
     /// <summary>
     /// 注文情報が見つからなかったことを示すイベントID
     /// </summary>
-    internal static readonly EventId OrderNotFound = new(1401, nameof(OrderNotFound));
+    internal static readonly EventId OrderNotFound = new(1301, nameof(OrderNotFound));
 
     /// <summary>
     /// デバッグ用のイベントID
