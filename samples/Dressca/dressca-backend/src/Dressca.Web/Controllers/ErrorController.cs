82f71ef0bdbdccbd02aa5df68fe1531e66c54bed
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-11-14 15:42:05 +0900

    XMLコメントの視点を呼び出し側に変更。 XMLコメントを敬体に変更。

--- samples/Dressca/dressca-backend/src/Dressca.Web/Controllers/ErrorController.cs
+++ samples/Dressca/dressca-backend/src/Dressca.Web/Controllers/ErrorController.cs
@@ -13,24 +13,22 @@ namespace Dressca.Web.Controllers;
 public class ErrorController : ControllerBase
 {
     /// <summary>
-    ///  開発環境におけるエラー情報取得のためのルートパスのリテラル値 ( /error-development ) 。
+    ///  開発環境におけるエラー情報取得のためのルートパスのリテラル値 ( /error-development ) です。
     /// </summary>
     private const string DevelopMentErrorRouteLiteral = "/error-development";
 
     /// <summary>
-    ///  実行環境におけるエラー情報取得のためのルートパスのリテラル値 ( /error ) 。
+    ///  実行環境におけるエラー情報取得のためのルートパスのリテラル値 ( /error ) です。
     /// </summary>
     private const string ErrorRouteLiteral = "/error";
 
     /// <summary>
-    ///  開発環境におけるエラー情報取得のためのルートパス（ /error-development ）を公開するためのプロパティ。。
-    ///  定数の公開を避けるためにプロパティ経由でアクセスする。
+    ///  開発環境におけるエラー情報取得のためのルートパス（ /error-development ）を取得します。
     /// </summary>
     public static string DevelopmentErrorRoute => DevelopMentErrorRouteLiteral;
 
     /// <summary>
-    ///  実行環境におけるエラー情報取得のためのルートパス ( /error )を公開するためのプロパティ。
-    ///  定数の公開を避けるためにプロパティ経由でアクセスする。
+    ///  実行環境におけるエラー情報取得のためのルートパス ( /error )を取得します。
     /// </summary>
     public static string ErrorRoute => ErrorRouteLiteral;
 
