449302cf1c54bca001782cb0f30cd74b557da534
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-12-09 16:56:43 +0900

    名前空間が物理フォルダと一致していない箇所を修正

--- samples/Dressca/dressca-backend/tests/Dressca.UnitTests.Web.Admin/TestBase.cs
+++ samples/Dressca/dressca-backend/tests/Dressca.UnitTests.Web.Admin/TestBase.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
 using Xunit.Abstractions;
 
-namespace Dressca.UnitTests.Web;
+namespace Dressca.UnitTests.Web.Admin;
 
 public class TestBase
 {
