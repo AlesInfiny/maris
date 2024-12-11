f39b25f028c541a83f1b67ceea4aff94fd5469f6
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-12-11 17:45:59 +0900

    OpenAPI 仕様書の更新に伴いクライアントコードを生成（影響なし）

--- samples/Dressca/dressca-frontend/admin/src/generated/api-client/api/users-api.ts
+++ samples/Dressca/dressca-frontend/admin/src/generated/api-client/api/users-api.ts
@@ -23,6 +23,8 @@ import { DUMMY_BASE_URL, assertParamExists, setApiKeyToObject, setBasicAuthToObj
 import { BASE_PATH, COLLECTION_FORMATS, type RequestArgs, BaseAPI, RequiredError, operationServerMap } from '../base';
 // @ts-ignore
 import type { GetLoginUserResponse } from '../models';
+// @ts-ignore
+import type { ProblemDetails } from '../models';
 /**
  * UsersApi - axios parameter creator
  * @export
