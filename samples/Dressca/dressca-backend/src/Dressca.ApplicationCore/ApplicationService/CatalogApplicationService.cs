1b67595cec326a63ab4d713014f65db7e40121f6
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-12-16 16:32:34 +0900

    Admin由来の軽微な改善を実施 (#2138)
    * API 仕様書のタイトルと説明をConsumerとわかるように変更
    
    * ApplicationServiceのGetCatalogItemsAsyncの検索条件の冗長な実装を修正

--- samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/ApplicationService/CatalogApplicationService.cs
+++ samples/Dressca/dressca-backend/src/Dressca.ApplicationCore/ApplicationService/CatalogApplicationService.cs
@@ -1,4 +1,5 @@
-﻿using Dressca.ApplicationCore.Catalog;
+﻿using System.Linq.Expressions;
+using Dressca.ApplicationCore.Catalog;
 using Dressca.ApplicationCore.Resources;
 using Microsoft.Extensions.Logging;
 
@@ -58,18 +59,11 @@ public class CatalogApplicationService
         int totalItems;
         using (var scope = TransactionScopeManager.CreateTransactionScope())
         {
-            itemsOnPage = await this.catalogRepository.FindAsync(
-                item =>
-                    (!brandId.HasValue || item.CatalogBrandId == brandId) &&
-                    (!categoryId.HasValue || item.CatalogCategoryId == categoryId),
-                skip,
-                take,
-                cancellationToken);
-            totalItems = await this.catalogRepository.CountAsync(
-                item =>
-                    (!brandId.HasValue || item.CatalogBrandId == brandId) &&
-                    (!categoryId.HasValue || item.CatalogCategoryId == categoryId),
-                cancellationToken);
+            Expression<Func<CatalogItem, bool>> specification = item =>
+                (!brandId.HasValue || item.CatalogBrandId == brandId) &&
+                (!categoryId.HasValue || item.CatalogCategoryId == categoryId);
+            itemsOnPage = await this.catalogRepository.FindAsync(specification, skip, take, cancellationToken);
+            totalItems = await this.catalogRepository.CountAsync(specification, cancellationToken);
             scope.Complete();
         }
 
