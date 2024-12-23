59a03f7744f5e55b6a2e838af2903584490b98d1
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-12-23 18:23:25 +0900

    アイテム一覧画面・アイテム追加画面のテストをAAAパターンに従うようにリファクタ

--- samples/Dressca/dressca-frontend/admin/src/views/__tests__/ItemsAddView.spec.ts
+++ samples/Dressca/dressca-frontend/admin/src/views/__tests__/ItemsAddView.spec.ts
@@ -1,23 +1,43 @@
-import { describe, it, expect, vi } from 'vitest';
-import { flushPromises, mount } from '@vue/test-utils';
+import { describe, it, expect, vi, beforeAll } from 'vitest';
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils';
 import { router } from '@/router';
 import { createPinia, setActivePinia } from 'pinia';
 import { createCustomErrorHandler } from '@/shared/error-handler/custom-error-handler';
 import ItemsAddView from '@/views/catalog/ItemsAddView.vue';
 
-describe('アイテム追加画面のテスト', () => {
-  it('アイテムを追加できる', async () => {
-    const pinia = createPinia();
-    setActivePinia(pinia);
-    const customErrorHandler = createCustomErrorHandler();
-    const wrapper = mount(ItemsAddView, {
-      global: { plugins: [pinia, router, customErrorHandler] },
-    });
+async function getWrapper() {
+  const pinia = createPinia();
+  setActivePinia(pinia);
+  const customErrorHandler = createCustomErrorHandler();
+  return mount(ItemsAddView, {
+    global: { plugins: [pinia, router, customErrorHandler] },
+  });
+}
+
+describe('アイテムを追加できる', () => {
+  let wrapper: VueWrapper;
+
+  beforeAll(async () => {
+    wrapper = await getWrapper();
+  });
+
+  it('追加画面に遷移できる', async () => {
+    // Arrange
+    // Act
     await flushPromises();
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテム追加');
+  });
+
+  it('追加ボタンを押下_追加成功_通知モーダルが開く', async () => {
+    // Arrange
+    // Act
     wrapper.find('button').trigger('click');
     await flushPromises();
-    await vi.waitUntil(() => wrapper.findAllComponents({ name: 'NotificationModal' })[0].isVisible())
+    await vi.waitUntil(() =>
+      wrapper.findAllComponents({ name: 'NotificationModal' })[0].isVisible(),
+    );
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテムを追加しました。');
   });
 });
--- samples/Dressca/dressca-frontend/admin/src/views/__tests__/ItemsView.spec.ts
+++ samples/Dressca/dressca-frontend/admin/src/views/__tests__/ItemsView.spec.ts
@@ -1,19 +1,30 @@
-import { describe, it, expect } from 'vitest';
-import { flushPromises, mount } from '@vue/test-utils';
+import { describe, it, expect, beforeAll } from 'vitest';
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils';
 import { router } from '@/router';
 import { createPinia, setActivePinia } from 'pinia';
 import { createCustomErrorHandler } from '@/shared/error-handler/custom-error-handler';
 import ItemsView from '@/views/catalog/ItemsView.vue';
 
-describe('アイテム一覧画面のテスト', () => {
-  it('アイテム一覧が表示できる', async () => {
-    const pinia = createPinia();
-    setActivePinia(pinia);
-    const customErrorHandler = createCustomErrorHandler();
-    const wrapper = mount(ItemsView, {
-      global: { plugins: [pinia, router, customErrorHandler] },
-    });
+async function getWrapper() {
+  const pinia = createPinia();
+  setActivePinia(pinia);
+  const customErrorHandler = createCustomErrorHandler();
+  return mount(ItemsView, {
+    global: { plugins: [pinia, router, customErrorHandler] },
+  });
+}
+describe('アイテム一覧が表示できる', () => {
+  let wrapper: VueWrapper;
+
+  beforeAll(async () => {
+    wrapper = await getWrapper();
+  });
+
+  it('アイテム一覧画面に遷移できる', async () => {
+    // Arrange
+    // Act
     await flushPromises();
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテム一覧');
   });
 });
