803a8d31620b2923f8e68544466636edb1296a42
kentah <158997636+KentaHizume@users.noreply.github.com>
2024-12-24 18:25:16 +0900

    フロントエンドの自動テストスイートを改善してmsw@2.7にアップデートする (#2169)
    * msw@2.7.0にマイナーアップデート
    
    * 確認対象のコンポーネントが描画されるまで待つように修正
    
    * アイテム編集画面のテストをAAAパターンに沿うようにリファクタリング
    
    * アイテム一覧画面・アイテム追加画面のテストをAAAパターンに従うようにリファクタ
    
    * 取得した個数分アイテムが表示されることを検証するケースを追加
    
    * lint漏れに対応

--- samples/Dressca/dressca-frontend/admin/src/views/__tests__/ItemsEditView.spec.ts
+++ samples/Dressca/dressca-frontend/admin/src/views/__tests__/ItemsEditView.spec.ts
@@ -1,71 +1,130 @@
-import { describe, it, expect } from 'vitest';
-import { flushPromises, mount } from '@vue/test-utils';
+import { describe, it, expect, vi, beforeAll } from 'vitest';
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils';
 import { createPinia, setActivePinia } from 'pinia';
 import { createCustomErrorHandler } from '@/shared/error-handler/custom-error-handler';
 import ItemsEditView from '@/views/catalog/ItemsEditView.vue';
 import { router } from '@/router';
 
-describe('アイテム編集画面_アイテム削除機能のテスト', () => {
-  it('アイテムを削除できる', async () => {
-    const pinia = createPinia();
-    setActivePinia(pinia);
-    const customErrorHandler = createCustomErrorHandler();
-    router.push({ name: 'catalog/items/edit', params: { itemId: 1 } });
-    await router.isReady();
-    const wrapper = mount(ItemsEditView, {
-      global: { plugins: [pinia, router, customErrorHandler] },
-    });
+async function getWrapper() {
+  const pinia = createPinia();
+  setActivePinia(pinia);
+  const customErrorHandler = createCustomErrorHandler();
+  router.push({ name: 'catalog/items/edit', params: { itemId: 1 } });
+  await router.isReady();
+  return mount(ItemsEditView, {
+    global: { plugins: [pinia, router, customErrorHandler] },
+  });
+}
+
+describe('アイテムが削除できる', () => {
+  let wrapper: VueWrapper;
+
+  beforeAll(async () => {
+    wrapper = await getWrapper();
+  });
+
+  it('編集画面に遷移できる', async () => {
+    // Arrange
+    // Act
     await flushPromises();
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテム編集');
+  });
+
+  it('削除ボタンを押下_確認モーダルが開く', async () => {
+    // Arrange
+    // Act
     await wrapper.findAll('button')[0].trigger('click');
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテムを削除します。');
+  });
+
+  it('OKボタンを押下_確認モーダルが閉じる', async () => {
+    // Arrange
+    // Act
     await wrapper
       .findAllComponents({ name: 'ConfirmationModal' })[0]
       .findAll('button')[0]
       .trigger('click');
     await flushPromises();
+    // Assert
     expect(
       wrapper.findAllComponents({ name: 'ConfirmationModal' })[0].isVisible(),
     ).toBeFalsy();
+  });
+
+  it('削除成功_通知モーダルが開く', async () => {
+    // Arrange
+    // Act
+    await vi.waitUntil(() =>
+      wrapper.findAllComponents({ name: 'NotificationModal' })[0].isVisible(),
+    );
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテムを削除しました。');
+  });
+
+  it('OKボタンを押下__通知モーダルが閉じる', async () => {
+    // Act
     await wrapper
       .findAllComponents({ name: 'NotificationModal' })[0]
       .findAll('button')[0]
       .trigger('click');
+    // Assert
     expect(
       wrapper.findAllComponents({ name: 'NotificationModal' })[0].isVisible(),
     ).toBeFalsy();
   });
 });
 
-describe('アイテム編集画面_アイテム更新機能のテスト', () => {
-  it('アイテムを更新できる', async () => {
-    const pinia = createPinia();
-    setActivePinia(pinia);
-    const customErrorHandler = createCustomErrorHandler();
-    router.push({ name: 'catalog/items/edit', params: { itemId: 1 } });
-    await router.isReady();
-    const wrapper = mount(ItemsEditView, {
-      global: { plugins: [pinia, router, customErrorHandler] },
-    });
+describe('アイテムが更新できる', () => {
+  let wrapper: VueWrapper;
+
+  beforeAll(async () => {
+    wrapper = await getWrapper();
+  });
+
+  it('編集画面に遷移できる', async () => {
+    // Arrange
+    // Act
     await flushPromises();
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテム編集');
+  });
+
+  it('更新ボタンを押下__確認モーダルが開く', async () => {
+    // Arrange
     const editButton = wrapper.findAll('button')[1];
+    // Act
     await editButton.trigger('click');
+    // Assert
     expect(wrapper.html()).toContain('カタログアイテムを更新します。');
+  });
+
+  it('更新成功__通知モーダルが開く', async () => {
+    // Arrange
+    // Act
     await wrapper
       .findAllComponents({ name: 'ConfirmationModal' })[1]
       .findAll('button')[0]
       .trigger('click');
     await flushPromises();
+    await vi.waitUntil(() =>
+      wrapper.findAllComponents({ name: 'NotificationModal' })[1].isVisible(),
+    );
+    // Assert
     expect(
       wrapper.findAllComponents({ name: 'ConfirmationModal' })[1].isVisible(),
     ).toBeFalsy();
     expect(wrapper.html()).toContain('カタログアイテムを更新しました。');
+  });
+
+  it('OKボタンを押下__通知モーダルが閉じる', async () => {
+    // Act
     await wrapper
       .findAllComponents({ name: 'NotificationModal' })[1]
       .findAll('button')[0]
       .trigger('click');
+    // Assert
     expect(
       wrapper.findAllComponents({ name: 'NotificationModal' })[1].isVisible(),
     ).toBeFalsy();
