cb6348958a60c907f353204cbf001baf12296cbf
Fumika Koyama <fkoyama3@ixas.unisys.co.jp>
2025-01-22 17:28:12 +0900

    リポジトリの引数に渡されるオブジェクトのプロパティ値を検証するテストを追加。

--- samples/Dressca/dressca-backend/tests/Dressca.UnitTests.ApplicationCore/ApplicationService/ShoppingApplicationServiceTest.cs
+++ samples/Dressca/dressca-backend/tests/Dressca.UnitTests.ApplicationCore/ApplicationService/ShoppingApplicationServiceTest.cs
@@ -344,6 +344,44 @@ public class ShoppingApplicationServiceTest(ITestOutputHelper testOutputHelper) 
             Times.Once);
     }
 
+    [Fact]
+    public async Task AddItemToBasketAsync_カタログリポジトリに存在するカタログアイテムが追加対象_買い物かごに追加対象の商品が追加される()
+    {
+        // Arrange
+        var dummyBuyerId = "dummyId";
+        var dummyBasket = new Basket { BuyerId = dummyBuyerId };
+        var quantities = new Dictionary<long, int>() { { 10L, 5 } };
+        var catalogItem = new CatalogItem() { CatalogCategoryId = 100L, CatalogBrandId = 110L, Description = "説明1", Name = "ダミー商品1", Price = 1000m, ProductCode = "C000000001", Id = 10L };
+        var catalogItems = new List<CatalogItem> { catalogItem };
+
+        var basketRepo = new Mock<IBasketRepository>();
+        basketRepo
+            .Setup(r => r.GetWithBasketItemsAsync(dummyBuyerId, AnyToken))
+            .ReturnsAsync(dummyBasket);
+        var orderRepo = Mock.Of<IOrderRepository>();
+        var orderFactory = Mock.Of<IOrderFactory>();
+        var catalogRepo = Mock.Of<ICatalogRepository>();
+        var catalogDomainService = new Mock<ICatalogDomainService>();
+        catalogDomainService
+            .Setup(d => d.ExistsAllAsync(quantities.Keys, AnyToken))
+            .ReturnsAsync((true, catalogItems.AsReadOnly()));
+        var logger = this.CreateTestLogger<ShoppingApplicationService>();
+        var service = new ShoppingApplicationService(basketRepo.Object, orderRepo, orderFactory, catalogRepo, catalogDomainService.Object, logger);
+
+        // Act
+        await service.AddItemToBasketAsync(dummyBuyerId, 10L, 5);
+
+        // Assert
+        basketRepo.Verify(
+            r => r.UpdateAsync(
+            It.Is<Basket>(b => b.Items.Count == 1), AnyToken),
+            Times.Once);
+        basketRepo.Verify(
+            r => r.UpdateAsync(
+            It.Is<Basket>(b => b.Items.ToList().Exists(item => (item.CatalogItemId == catalogItem.Id) && (item.Quantity == 5) && (item.UnitPrice == catalogItem.Price))), AnyToken),
+            Times.Once);
+    }
+
     [Theory]
     [InlineData(null)]
     [InlineData("")]
@@ -509,6 +547,52 @@ public class ShoppingApplicationServiceTest(ITestOutputHelper testOutputHelper) 
         // Assert
         orderRepo.Verify(
             r => r.AddAsync(It.Is<Order>(o => o.BuyerId == dummyBuyerId), AnyToken),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task CheckoutAsync_買い物かごにアイテムが存在する_注文リポジトリのAddAsyncの引数にお届け先が正しく設定されている()
+    {
+        // Arrange
+        var dummyBuyerId = "dummyId";
+        var dummyBasket = new Basket { BuyerId = dummyBuyerId };
+        dummyBasket.AddItem(10L, 1000);
+        var shipTo = CreateDefaultShipTo();
+        var catalogItems = new List<CatalogItem>
+         {
+             new() { CatalogCategoryId = 100L, CatalogBrandId = 110L, Description = "説明1", Name = "ダミー商品1", Price = 1000m, ProductCode = "C000000001", Id = 10L },
+         };
+        var order = new Order(CreateDefaultOrderItems()) { BuyerId = dummyBuyerId, ShipToAddress = shipTo };
+
+        var basketRepo = new Mock<IBasketRepository>();
+        basketRepo
+            .Setup(r => r.GetWithBasketItemsAsync(dummyBuyerId, AnyToken))
+            .ReturnsAsync(dummyBasket);
+        basketRepo
+            .Setup(r => r.RemoveAsync(dummyBasket, AnyToken))
+            .Returns(Task.CompletedTask);
+        var orderRepo = new Mock<IOrderRepository>();
+        orderRepo
+            .Setup(r => r.AddAsync(order, AnyToken))
+            .ReturnsAsync(order);
+        var orderFactory = new Mock<IOrderFactory>();
+        orderFactory
+            .Setup(f => f.CreateOrder(dummyBasket, catalogItems, shipTo))
+            .Returns(order);
+        var catalogRepo = new Mock<ICatalogRepository>();
+        catalogRepo
+            .Setup(r => r.FindAsync(It.IsAny<Expression<Func<CatalogItem, bool>>>(), AnyToken))
+            .ReturnsAsync(catalogItems.AsReadOnly());
+        var catalogDomainService = Mock.Of<ICatalogDomainService>();
+        var logger = this.CreateTestLogger<ShoppingApplicationService>();
+        var service = new ShoppingApplicationService(basketRepo.Object, orderRepo.Object, orderFactory.Object, catalogRepo.Object, catalogDomainService, logger);
+
+        // Act
+        await service.CheckoutAsync(dummyBuyerId, shipTo);
+
+        // Assert
+        orderRepo.Verify(
+            r => r.AddAsync(It.Is<Order>(o => o.ShipToAddress == shipTo), AnyToken),
             Times.Once);
     }
 
