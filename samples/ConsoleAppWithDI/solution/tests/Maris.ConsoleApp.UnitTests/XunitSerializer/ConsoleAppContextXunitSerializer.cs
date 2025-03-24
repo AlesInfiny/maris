0df7350fe89a6510423f45341ab4a969d9b89207
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-03-24 22:30:02 +0900

    XUnit用の新しいシリアライザークラスを追加
    `CommandBaseXunitSerializer` と `ConsoleAppContextXunitSerializer` を追加し、`JsonXunitSerializer` を継承して XUnit テストでのシリアライズおよびデシリアライズをサポートします。また、`JsonXunitSerializer<T>` クラスにより、指定された型のオブジェクトを JSON 形式で処理できるようになり、エラーメッセージも日本語で提供されます。

---
+++ samples/ConsoleAppWithDI/solution/tests/Maris.ConsoleApp.UnitTests/XunitSerializer/ConsoleAppContextXunitSerializer.cs
@@ -0,0 +1,11 @@
+﻿using Maris.ConsoleApp.Core;
+using Maris.ConsoleApp.UnitTests.XunitSerializer;
+using Xunit.Sdk;
+
+[assembly: RegisterXunitSerializer(typeof(ConsoleAppContextXunitSerializer), typeof(ConsoleAppContext))]
+
+namespace Maris.ConsoleApp.UnitTests.XunitSerializer;
+
+public class ConsoleAppContextXunitSerializer : JsonXunitSerializer<ConsoleAppContext>
+{
+}
---
+++ samples/ConsoleAppWithDI/solution/tests/Maris.ConsoleApp.UnitTests/XunitSerializer/JsonXunitSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using Xunit.Sdk;
+
+namespace Maris.ConsoleApp.UnitTests.XunitSerializer;
+
+public class JsonXunitSerializer<T> : IXunitSerializer
+{
+    public object Deserialize(Type type, string serializedValue)
+    {
+        if (type == typeof(T))
+        {
+            var context = JsonSerializer.Deserialize<T>(serializedValue);
+            if (context is not null)
+            {
+                return context;
+            }
+        }
+
+        throw new NotSupportedException("デシリアライズに失敗しました。");
+    }
+
+    public bool IsSerializable(Type type, object? value, [NotNullWhen(false)] out string? failureReason)
+    {
+        if (type == typeof(T))
+        {
+            failureReason = null;
+            return true;
+        }
+
+        failureReason = "サポートされていない型です。";
+        return false;
+    }
+
+    public string Serialize(object value)
+        => JsonSerializer.Serialize(value);
+}
