e2f965c01983d1fec575ae08dad2f550bebc83c0
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-03-25 19:12:37 +0900

    テストメソッドにキャンセルトークンを追加
    `ApiTest.cs`のテストメソッドでHTTPリクエストを送信する際に、キャンセルトークンを追加しました。これにより、リクエストがキャンセル可能になり、テストの柔軟性が向上しました。具体的には、`GetAsync`メソッドの呼び出しに`cancellationToken`を渡すように変更され、認証が必要なAPIおよび認証不要なAPIの両方でリクエストがキャンセルできるようになりました。

--- samples/AzureADB2CAuth/auth-backend/tests/Dressca.IntegrationTest/ApiTest.cs
+++ samples/AzureADB2CAuth/auth-backend/tests/Dressca.IntegrationTest/ApiTest.cs
@@ -14,10 +14,11 @@ public class ApiTest(ApiTestWebApplicationFactory<Program> factory)
     {
         // Arrange
         var client = this.factory.CreateClient();
+        var cancellationToken = TestContext.Current.CancellationToken;
 
         // Act
         // 認証せずにリクエスト送信
-        var response = await client.GetAsync("api/users");
+        var response = await client.GetAsync("api/users", cancellationToken);
 
         // Assert
         Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
@@ -31,13 +32,14 @@ public class ApiTest(ApiTestWebApplicationFactory<Program> factory)
         // ログイン成功時に取得するJWTをヘッダーに追加
         var token = this.factory.CreateToken("testUser");
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var cancellationToken = TestContext.Current.CancellationToken;
 
         // Act
-        var response = await client.GetAsync("api/users");
+        var response = await client.GetAsync("api/users", cancellationToken);
 
         // Assert
         response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync(cancellationToken);
         Assert.Equal("{\"userId\":\"testUser\"}", result);
     }
 
@@ -46,13 +48,14 @@ public class ApiTest(ApiTestWebApplicationFactory<Program> factory)
     {
         // Arrange
         var client = this.factory.CreateClient();
+        var cancellationToken = TestContext.Current.CancellationToken;
 
         // Act
-        var response = await client.GetAsync("api/servertime");
+        var response = await client.GetAsync("api/serverTime", cancellationToken);
 
         // Assert
         response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync(cancellationToken);
         // "{"serverTime":"2024/04/12 13:32:59"}"
         Assert.StartsWith("{\"serverTime\":", result);
     }
