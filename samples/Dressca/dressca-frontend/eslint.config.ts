fffe9382c332a125abd3b74da84784b4394b0360
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-07-31 22:51:33 +0900

    ルートプロジェクト直下を移植

---
+++ samples/Dressca/dressca-frontend/eslint.config.ts
@@ -0,0 +1,86 @@
+import { globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import pluginVitest from '@vitest/eslint-plugin'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import pluginCypress from 'eslint-plugin-cypress/flat'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  // Lint 対象外とするファイルパスを列挙します。
+  globalIgnores([
+    '**/dist/**',
+    '**/dist-ssr/**',
+    '**/coverage/**',
+    '**/src/generated/**',
+    '**/mockServiceWorker.js',
+  ]),
+
+  // Vue.js 向けの推奨ルールを適用します。
+  // .vue ファイルを Lint の対象とします。
+  pluginVue.configs['flat/recommended'],
+
+  // TypeScript + Vue.js 向けの型情報を使用した推奨ルールを適用します。
+  // .vue .ts .mts .ts .vue ファイルを Lint の対象とします。
+  vueTsConfigs.recommendedTypeChecked,
+
+  // 型情報を使用した Lint を実行するために、 tsconfig ファイルを探すための設定をします。
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  // JavaScript ファイルに対しては、 型情報を使用した Lint は無効化します。
+  {
+    files: ['**/*.js'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+
+  // プロジェクトやワークスペースに固有のルールを適用します。
+  // 必要に応じて対象のファイルやルールを設定します。
+  {
+    name: 'dressca-frontend/additional-rules',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      'no-console': 'warn',
+      'no-alert': 'warn',
+      '@typescript-eslint/no-floating-promises': [
+        'error',
+        {
+          // 戻り値の Promise を await 不要とみなすメソッドを例外登録します。
+          allowForKnownSafeCalls: [
+            { from: 'package', name: ['push', 'replace'], package: 'vue-router' },
+          ],
+        },
+      ],
+    },
+  },
+
+  // Vitest 用のテストスイートに対して、 Vitest 推奨の Lint ルールを適用します。
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['**/src/**/__tests__/**/*'],
+  },
+
+  // Cypress 用のテストスイートに対して、Cypress 推奨の Lint ルールを適用します。
+  {
+    ...pluginCypress.configs.recommended,
+    files: [
+      '**/cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+      '**/cypress/support/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
+  // コードのフォーマットは Prettier で実行するので、 ESLint のフォーマットルールは無効化します。
+  skipFormatting,
+)
