b45ad41dcca28db957fbe5ade5ac73e6cfe75388
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-10-22 15:03:11 +0900

    ルートプロジェクト直下を移植

--- samples/Dressca/dressca-frontend/.stylelintrc.js
+++ samples/Dressca/dressca-frontend/.stylelintrc.js
@@ -1,12 +1,40 @@
 export default {
   extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue'],
   rules: {
-    'at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind', 'define-mixin'] }],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          /**
+           * Tailwind CSS 固有のアットルール（ディレクティブ）をホワイトリストに登録します。
+           * https://tailwindcss.com/docs/functions-and-directives#directives
+           **/
+          'theme',
+          'source',
+          'utility',
+          'variant',
+          'custom-variant',
+          'apply',
+          'reference',
+        ],
+      },
+    ],
+    'function-no-unknown': [
+      true,
+      {
+        /**
+         * Tailwind CSS が提供する関数をホワイトリストに登録します。
+         * https://tailwindcss.com/docs/functions-and-directives#functions
+         **/
+        ignoreFunctions: ['alpha', 'spacing'],
+      },
+    ],
   },
-  ignoreFiles: ['dist/**/*'],
+  ignoreFiles: ['dist/**/*', 'coverage/**/*'],
   overrides: [
     {
       files: ['**/*.vue'],
+      /** Vue ファイルの <style> ブロック内を Lint するための設定です。*/
       customSyntax: 'postcss-html',
     },
   ],
