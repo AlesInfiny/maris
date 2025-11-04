79245db1c09aed873fa06d4ea30cd83ca323542c
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-11-04 15:44:25 +0900

    下記のINFOレベルのコード分析に対処 結合テスト用にProgramクラスを公開する必要はなくなったので削除する。 https://learn.microsoft.com/ja-jp/aspnet/core/diagnostics/asp0027?view=aspnetcore-10.0

--- samples/Dressca/dressca-backend/src/Dressca.Web.Consumer/Program.cs
+++ samples/Dressca/dressca-backend/src/Dressca.Web.Consumer/Program.cs
@@ -141,10 +141,3 @@ app.MapHealthChecks(HealthCheckDescriptionProvider.HealthCheckRelativePath);
 app.MapFallbackToFile("/index.html");
 
 app.Run();
-
-/// <summary>
-///  結合テストプロジェクト用の部分クラス。
-/// </summary>
-public partial class Program
-{
-}
