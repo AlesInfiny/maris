4a2774a2b1a36ec8b9d7b0e9a539a02cd953d636
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-11-27 22:20:19 +0900

    ビルド時の警告に対処

--- samples/DresscaCMS/src/DresscaCMS.Web/Program.cs
+++ samples/DresscaCMS/src/DresscaCMS.Web/Program.cs
@@ -1,4 +1,4 @@
-using DresscaCMS.Web.Components;
+﻿using DresscaCMS.Web.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,9 +14,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
