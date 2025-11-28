ff5156223f9f55975912a8941f02bd28fd58b1f4
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-11-28 16:52:35 +0900

    認証機能のプロジェクトを追加

---
+++ samples/DresscaCMS/src/DresscaCMS.Authentication/Infrastructures/AuthenticationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using DresscaCMS.Authentication.Infrastructures.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DresscaCMS.Authentication.Infrastructures;
+
+public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
+{
+
+    public AuthenticationDbContext()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AppIdentityDbContext"/> class.
+    /// </summary>
+    /// <param name="options">DB コンテキストオプション.</param>
+    public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+        : base(options)
+    {
+    }
+
+    /// <inheritdoc/>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Dressca.Cms.Authentication;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        base.OnModelCreating(builder);
+    }
+
+}
