5d1eb50bde7b6d3688a66f8fe4bf731809c266c6
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-11-28 10:57:20 +0900

    業務名でプロジェクトを命名するようにソリューション構造を変更

--- samples/DresscaCMS/src/DresscaCMS.Announcement/Resources/Messages.Designer.cs
+++ samples/DresscaCMS/src/DresscaCMS.Announcement/Resources/Messages.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DresscaCMS.ApplicationCore.Resources {
+namespace DresscaCMS.Announcement.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DresscaCMS.ApplicationCore.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DresscaCMS.ApplicationCore.Resources.Messages", typeof(Messages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DresscaCMS.Announcement.Resources.Messages", typeof(Messages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
