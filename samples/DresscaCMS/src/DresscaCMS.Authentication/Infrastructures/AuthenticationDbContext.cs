6e55f40f1afd0011799cd99325009cce8f0c3f5f
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-12-04 12:22:17 +0900

    認証用のプロジェクトを追加

---
+++ samples/DresscaCMS/src/DresscaCMS.Authentication/Infrastructures/AuthenticationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Emit;
+using DresscaCMS.Authentication.Infrastructures.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DresscaCMS.Authentication.Infrastructures;
+
+public class AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+
+    /// <inheritdoc/>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Dressca.Cms.Authentication;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        base.OnModelCreating(builder);
+    }
+
+}
---
+++ samples/DresscaCMS/src/DresscaCMS.Authentication/Infrastructures/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DresscaCMS.Authentication.Infrastructures.Entities;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
