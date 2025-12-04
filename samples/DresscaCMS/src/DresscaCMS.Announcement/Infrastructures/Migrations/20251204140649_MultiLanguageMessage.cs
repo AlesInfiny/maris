a8989a47979caac90b2d16be700d1e1f9adf1a95
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-12-04 23:08:06 +0900

    多言語のデータを投入

---
+++ samples/DresscaCMS/src/DresscaCMS.Announcement/Infrastructures/Migrations/20251204140649_MultiLanguageMessage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DresscaCMS.Announcement.Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class MultiLanguageMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AnnouncementContents",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222218"),
+                columns: new[] { "LanguageCode", "Message", "Title" },
+                values: new object[] { "es", "Detalles 18", "Anuncio 18" });
+
+            migrationBuilder.UpdateData(
+                table: "AnnouncementContents",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222219"),
+                columns: new[] { "LanguageCode", "Message", "Title" },
+                values: new object[] { "zh", "详情 19", "公告 19" });
+
+            migrationBuilder.UpdateData(
+                table: "AnnouncementContents",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222220"),
+                columns: new[] { "LanguageCode", "Message", "Title" },
+                values: new object[] { "en", "Details 20", "Notice 20" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AnnouncementContents",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222218"),
+                columns: new[] { "LanguageCode", "Message", "Title" },
+                values: new object[] { "ja", "内容 18", "お知らせ 18" });
+
+            migrationBuilder.UpdateData(
+                table: "AnnouncementContents",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222219"),
+                columns: new[] { "LanguageCode", "Message", "Title" },
+                values: new object[] { "ja", "内容 19", "お知らせ 19" });
+
+            migrationBuilder.UpdateData(
+                table: "AnnouncementContents",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222220"),
+                columns: new[] { "LanguageCode", "Message", "Title" },
+                values: new object[] { "ja", "内容 20", "お知らせ 20" });
+        }
+    }
+}
