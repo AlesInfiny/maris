f322ff92a29f786a271a949636e229d011937039
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-12-10 09:41:44 +0900

    インメモリでコンポーネント間の情報引継ぎができる機能を追加する (#4009)
    * StateResult<T> クラスとその単体テストを追加
    
    StateResult.cs にジェネリック型 T を使用した StateResult<T> クラスを追加。このクラスは、ステートの取得結果を表現するためのレコード型であり、以下の機能を提供:
    - Found プロパティ: ステートが見つかったかどうかを示す。
    - Value プロパティ: ステートの値を保持。
    - NotFound メソッド: ステートが見つからなかった場合のインスタンスを生成。
    - FoundValue メソッド: ステートが見つかった場合のインスタンスを生成。
    
    StateResultTest.cs に StateResult<T> クラスの単体テストを追加:
    - NotFound メソッドの挙動を検証。
    - FoundValue メソッドの挙動を検証 (値型、参照型、dynamic 型のサポートを含む)。
    
    * `InternalsVisibleTo` を追加してテストプロジェクトを許可
    
    `DresscaCMS.Web.csproj` に `<InternalsVisibleTo>` タグを追加しました。
    これにより、`DresscaCMS.UnitTests.Web` プロジェクトが `DresscaCMS.Web` プロジェクトの内部メンバーにアクセス可能になり、ユニットテストの実行が容易になります。
    
    * 揮発性ステート管理とリソース対応の追加
    
    `IConsumableStateStore` インターフェースを追加し、`InMemoryConsumableStateStore` クラスでその実装を提供しました。これにより、キーと値のペアを管理する揮発性ステートストアの操作が可能になりました。
    
    また、`Messages.resx` と `Messages.Designer.cs` を追加し、ローカライズされたリソース文字列を型安全に取得できる仕組みを導入しました。新しいリソースキー `StoredObjectCannotCastTo` を追加し、型変換エラー時のローカライズ対応を強化しました。
    
    さらに、`InMemoryConsumableStateStore` の単体テストを実装し、主要な操作（`ClearAsync`、`PeekAsync`、`PopAsync`、`RemoveAsync`、`SetAsync`）の動作を検証しました。
    
    * DIコンテナーに登録できるように、クラスとコンストラクターのアクセスレベルを修正
    
    * InMemoryConsumableStateStoreをDIコンテナーに登録
    
    `Program.cs` に `AddInMemoryConsumableStateStore` を追加し、依存性注入コンテナに「インメモリ消費可能ステートストア」サービスを登録しました。
    
    `StateServicesExtensions.cs` に新しい拡張メソッド `AddInMemoryConsumableStateStore` を追加しました。このメソッドは、`IConsumableStateStore` のインメモリ実装をスコープ付きサービスとして登録します。
    
    また、StyleCop のルール `SA1101` を無効化するための `SuppressMessage` 属性を追加しました。コメントには日本語での説明を記載し、拡張メソッドの目的や戻り値について詳細に説明しています。
    
    * クラス名、メソッド名をシンプルなものに変更
    
    * `GetAsync` と `PopAsync` のラムダ式によるリファクタリング
    
    `GetAsync<T>` と `PopAsync<T>` メソッドをリファクタリングし、従来のブロック形式のコードを三項演算子を用いたラムダ式に変更しました。この変更により、コードが簡潔化され、可読性が向上しました。
    
    * XMLコメントの <list> 内の要素は <item> 要素で囲むように修正しました。
    該当箇所はリストの要素が 1つですが、 <item> タグを使用する方向で統一されています。
    
    ---------
    
    Co-authored-by: kentah <158997636+KentaHizume@users.noreply.github.com>

--- samples/DresscaCMS/src/DresscaCMS.Web/Program.cs
+++ samples/DresscaCMS/src/DresscaCMS.Web/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddFluentUIComponents();
 builder.Services.AddRazorPages();
+builder.Services.AddInMemoryStateStore();
 
 if (builder.Environment.IsDevelopment())
 {
