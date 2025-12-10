f6c9aa2324e12cd7ffc27623af58caff1d242974
kentah <158997636+KentaHizume@users.noreply.github.com>
2025-12-10 15:11:11 +0900

    表示優先度の管理をスタッシュ

--- samples/DresscaCMS/src/DresscaCMS.Announcement/ApplicationCore/DisplayPriority.cs
+++ samples/DresscaCMS/src/DresscaCMS.Announcement/ApplicationCore/DisplayPriority.cs
@@ -1,4 +1,6 @@
-﻿namespace DresscaCMS.Announcement.ApplicationCore;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DresscaCMS.Announcement.ApplicationCore;
 
 /// <summary>
 ///  お知らせメッセージの優先度を示す列挙型です。
@@ -6,14 +8,18 @@
 public enum DisplayPriority
 {
     /// <summary>緊急です。</summary>
+    [Display(Name = "緊急")]
     Critical = 1,
 
     /// <summary>高です。</summary>
+    [Display(Name = "高")]
     High = 2,
 
     /// <summary>中です。</summary>
+    [Display(Name = "中")]
     Medium = 3,
 
     /// <summary>低です。</summary>
+    [Display(Name = "低")]
     Low = 4,
 }
---
+++ samples/DresscaCMS/src/DresscaCMS.Announcement/ApplicationCore/DisplayPriorityExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace DresscaCMS.Announcement.ApplicationCore;
+
+/// <summary>
+///  <see cref="DisplayPriority"/> の拡張メソッドを提供します。
+/// </summary>
+public static class DisplayPriorityExtensions
+{
+    /// <summary>
+    ///  表示優先度の表示名を取得します。
+    /// </summary>
+    /// <param name="priority">表示優先度。</param>
+    /// <returns>表示名。</returns>
+    public static string ToDisplayName(this DisplayPriority priority)
+    {
+        return priority switch
+        {
+            DisplayPriority.Critical => "緊急",
+            DisplayPriority.High => "高",
+            DisplayPriority.Medium => "中",
+            DisplayPriority.Low => "低",
+            _ => priority.ToString(),
+        };
+    }
+}
