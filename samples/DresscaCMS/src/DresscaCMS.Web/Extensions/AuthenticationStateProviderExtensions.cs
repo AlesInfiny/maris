23db5c5d6d2784dfbefea8fd5f46ec629b716944
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2025-12-12 16:39:05 +0900

    ユーザー名取得メソッドを共通化

---
+++ samples/DresscaCMS/src/DresscaCMS.Web/Extensions/AuthenticationStateProviderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace DresscaCMS.Web.Extensions;
+
+/// <summary>
+///  <see cref="AuthenticationStateProvider"/> の拡張メソッドを提供します。
+/// </summary>
+public static class AuthenticationStateProviderExtensions
+{
+    /// <summary>
+    ///  現在ログイン中のユーザー名を取得します。
+    /// </summary>
+    /// <param name="authenticationStateProvider">認証状態プロバイダー。</param>
+    /// <param name="cancellationToken">キャンセルトークン。</param>
+    /// <returns>ユーザー名。取得できない場合は "不明なユーザー"。</returns>
+    public static async Task<string> GetCurrentUserNameAsync(
+        this AuthenticationStateProvider authenticationStateProvider,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(authenticationStateProvider);
+
+        var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity?.IsAuthenticated == true)
+        {
+            return user.Identity.Name ?? "不明なユーザー";
+        }
+
+        return "不明なユーザー";
+    }
+}
