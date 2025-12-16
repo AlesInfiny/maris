c6d1ba3f5acadb58a64623f7a5ef55c7d4c4ce90
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-12-16 12:52:04 +0900

    LanguagePriorities を IReadOnlyList に変更しAPI整理
    LanguagePriorities クラスを IList から IReadOnlyList に変更し、書き込み系メソッドやプロパティ（Add, Remove, Insert, Clear, IsReadOnly など）を削除しました。また、指定言語の表示順を取得する GetDisplayOrder メソッドを追加しました。その他、不要なメソッドの削除や IEnumerator 実装の整理を行いました。

--- samples/DresscaCMS/src/DresscaCMS.Web/ViewModels/LanguagePriorities.cs
+++ samples/DresscaCMS/src/DresscaCMS.Web/ViewModels/LanguagePriorities.cs
@@ -5,15 +5,12 @@ namespace DresscaCMS.Web.ViewModels;
 /// <summary>
 ///  言語の優先順位を表すリストを提供します。
 /// </summary>
-public class LanguagePriorities : IList<Language>
+public class LanguagePriorities : IReadOnlyList<Language>
 {
     private readonly List<Language> languages = [Language.Japanese, Language.English, Language.Chinese, Language.Spanish];
 
     /// <inheritdoc/>
     public int Count => this.languages.Count;
-
-    /// <inheritdoc/>
-    public bool IsReadOnly => true;
 
     /// <inheritdoc/>
     public Language this[int index]
@@ -22,33 +19,21 @@ public class LanguagePriorities : IList<Language>
         set => throw new NotSupportedException();
     }
 
-    /// <inheritdoc/>
-    public void Add(Language item) => throw new NotSupportedException();
-
-    /// <inheritdoc/>
-    public void Clear() => throw new NotSupportedException();
-
-    /// <inheritdoc/>
-    public bool Contains(Language item) => this.languages.Contains(item);
-
-    /// <inheritdoc/>
-    public void CopyTo(Language[] array, int arrayIndex) => this.languages.CopyTo(array, arrayIndex);
+    /// <summary>
+    ///  指定した言語の表示順を取得します。
+    ///  対象外の言語の場合は <see cref="int.MaxValue"/> を返します。
+    /// </summary>
+    /// <param name="language">言語。</param>
+    /// <returns>表示順。</returns>
+    public int GetDisplayOrder(Language language)
+    {
+        var index = this.languages.IndexOf(language);
+        return index >= 0 ? index : int.MaxValue;
+    }
 
     /// <inheritdoc/>
     public IEnumerator<Language> GetEnumerator() => this.languages.GetEnumerator();
 
     /// <inheritdoc/>
-    public int IndexOf(Language item) => this.languages.IndexOf(item);
-
-    /// <inheritdoc/>
-    public void Insert(int index, Language item) => throw new NotSupportedException();
-
-    /// <inheritdoc/>
-    public bool Remove(Language item) => throw new NotSupportedException();
-
-    /// <inheritdoc/>
-    public void RemoveAt(int index) => throw new NotSupportedException();
-
-    /// <inheritdoc/>
-    IEnumerator IEnumerable.GetEnumerator() => this.languages.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 }
