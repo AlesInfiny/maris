f4c567f0c1ee188949ebf825c74d3e41eb3e5048
Kaori ISHIKAWA <kaori.ishikawa@biprogy.com>
2026-01-05 17:06:41 +0900

    AnnoucementHistoryにダミーデータを追加

---
+++ samples/DresscaCMS/src/DresscaCMS.Announcement/Infrastructures/Migrations/20260105080415_UpdateAnnouncementSeedData.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DresscaCMS.Announcement.Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAnnouncementSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AnnouncementHistory",
+                columns: new[] { "Id", "AnnouncementId", "Category", "ChangedBy", "CreatedAt", "DisplayPriority", "ExpireDateTime", "OperationType", "PostDateTime" },
+                values: new object[,]
+                {
+                    { new Guid("33333333-3333-3333-3333-333333333301"), new Guid("11111111-1111-1111-1111-111111111111"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333302"), new Guid("11111111-1111-1111-1111-111111111112"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 2, new DateTimeOffset(new DateTime(2026, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333303"), new Guid("11111111-1111-1111-1111-111111111113"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 3, new DateTimeOffset(new DateTime(2026, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333304"), new Guid("11111111-1111-1111-1111-111111111114"), "イベント", "system", new DateTimeOffset(new DateTime(2025, 1, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 4, new DateTimeOffset(new DateTime(2026, 1, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333305"), new Guid("11111111-1111-1111-1111-111111111115"), "イベント", "system", new DateTimeOffset(new DateTime(2025, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2026, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333306"), new Guid("11111111-1111-1111-1111-111111111116"), "更新", "system", new DateTimeOffset(new DateTime(2025, 1, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 2, new DateTimeOffset(new DateTime(2026, 1, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333307"), new Guid("11111111-1111-1111-1111-111111111117"), "更新", "system", new DateTimeOffset(new DateTime(2025, 1, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 3, new DateTimeOffset(new DateTime(2026, 1, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333308"), new Guid("11111111-1111-1111-1111-111111111118"), "重要", "system", new DateTimeOffset(new DateTime(2025, 1, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 4, new DateTimeOffset(new DateTime(2026, 1, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333309"), new Guid("11111111-1111-1111-1111-111111111119"), "重要", "system", new DateTimeOffset(new DateTime(2025, 1, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2026, 1, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333310"), new Guid("11111111-1111-1111-1111-111111111120"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 3, new DateTimeOffset(new DateTime(2026, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333311"), new Guid("11111111-1111-1111-1111-111111111121"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 2, new DateTimeOffset(new DateTime(2026, 1, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333312"), new Guid("11111111-1111-1111-1111-111111111122"), "イベント", "system", new DateTimeOffset(new DateTime(2025, 1, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 4, new DateTimeOffset(new DateTime(2026, 1, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333313"), new Guid("11111111-1111-1111-1111-111111111123"), "イベント", "system", new DateTimeOffset(new DateTime(2025, 1, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2026, 1, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333314"), new Guid("11111111-1111-1111-1111-111111111124"), "更新", "system", new DateTimeOffset(new DateTime(2025, 1, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 3, new DateTimeOffset(new DateTime(2026, 1, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333315"), new Guid("11111111-1111-1111-1111-111111111125"), "更新", "system", new DateTimeOffset(new DateTime(2025, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 2, new DateTimeOffset(new DateTime(2026, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333316"), new Guid("11111111-1111-1111-1111-111111111126"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 4, new DateTimeOffset(new DateTime(2026, 1, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333317"), new Guid("11111111-1111-1111-1111-111111111127"), "重要", "system", new DateTimeOffset(new DateTime(2025, 1, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2026, 1, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333318"), new Guid("11111111-1111-1111-1111-111111111128"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 3, new DateTimeOffset(new DateTime(2026, 1, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333319"), new Guid("11111111-1111-1111-1111-111111111129"), "イベント", "system", new DateTimeOffset(new DateTime(2025, 1, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 2, new DateTimeOffset(new DateTime(2026, 1, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333320"), new Guid("11111111-1111-1111-1111-111111111130"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 3, new DateTimeOffset(new DateTime(2026, 1, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-3333-3333-3333-333333333321"), new Guid("11111111-1111-1111-1111-111111111131"), "一般", "system", new DateTimeOffset(new DateTime(2025, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 4, new DateTimeOffset(new DateTime(2026, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-4444-3333-3333-333333333301"), new Guid("11111111-2222-1111-1111-111111111111"), "テスト", "system", new DateTimeOffset(new DateTime(2025, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 4, new DateTimeOffset(new DateTime(2026, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { new Guid("33333333-5555-3333-3333-333333333301"), new Guid("11111111-3333-1111-1111-111111111111"), "テスト", "system", new DateTimeOffset(new DateTime(2025, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 4, new DateTimeOffset(new DateTime(2026, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), 1, new DateTimeOffset(new DateTime(2025, 1, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333301"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333302"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333303"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333304"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333305"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333306"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333307"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333308"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333309"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333310"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333311"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333312"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333313"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333314"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333315"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333316"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333317"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333318"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333319"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333320"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333321"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-4444-3333-3333-333333333301"));
+
+            migrationBuilder.DeleteData(
+                table: "AnnouncementHistory",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-5555-3333-3333-333333333301"));
+        }
+    }
+}
