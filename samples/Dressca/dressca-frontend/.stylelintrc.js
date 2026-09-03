a7afcbd14d3910ecf5924e90502818042e1f84b7
fkoyama <fumika.koyama@biprogy.com>
2026-09-03 11:47:19 +0900

    lintの設定修正

--- samples/Dressca/dressca-frontend/.stylelintrc.js
+++ samples/Dressca/dressca-frontend/.stylelintrc.js
@@ -30,7 +30,7 @@ export default {
       },
     ],
   },
-  ignoreFiles: ['dist/**/*', 'coverage/**/*'],
+  ignoreFiles: ['dist/**/*', 'coverage/**/*', '**/playwright-report/**/*'],
   overrides: [
     {
       files: ['**/*.vue'],
--- samples/Dressca/dressca-frontend/eslint.config.ts
+++ samples/Dressca/dressca-frontend/eslint.config.ts
@@ -21,6 +21,7 @@ export default defineConfigWithVueTs(
     '**/coverage/**',
     '**/src/generated/**',
     '**/mockServiceWorker.js',
+    '**/playwright-report/**'
   ]),
 
   // Vue.js 向けの推奨ルールを適用します。
